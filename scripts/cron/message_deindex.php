ee029809adf9406b3fb3e9cbbf1bb37686d3b898
Edward Hibbert <edward@ehibbert.org.uk>
2017-12-08 18:11:13 +0000

    Speed up indexing/deindexing

--- scripts/cron/message_deindex.php
+++ scripts/cron/message_deindex.php
@@ -7,16 +7,14 @@ require_once(IZNIK_BASE . '/include/message/Message.php');
 
 # Only interested in being able to search within the last 30 days.
 $date = date('Y-m-d', strtotime("30 days ago"));
-$msgs = $dbhr->preQuery("SELECT messages.id FROM messages INNER JOIN messages_groups ON messages_groups.msgid = messages.id AND messages_groups.collection = 'Approved' WHERE messages_groups.arrival < ? ORDER BY messages_groups.arrival DESC;", [
-    $date
-]);
+$msgs = $dbhr->query("SELECT DISTINCT messages_groups.msgid FROM messages_groups INNER JOIN messages_index ON messages_index.msgid = messages_groups.msgid WHERE messages_groups.collection = 'Approved' AND messages_groups.arrival < '$date' ORDER BY messages_groups.arrival DESC;");
 
 error_log("Deindex " . count($msgs));
 
 $done = 0;
 
 foreach ($msgs as $msg) {
-    $m = new Message($dbhr, $dbhm, $msg['id']);
+    $m = new Message($dbhr, $dbhm, $msg['msgid']);
     $m->deindex();
 
     $done++;
--- scripts/cron/message_unindexed.php
+++ scripts/cron/message_unindexed.php
@@ -7,12 +7,16 @@ require_once(IZNIK_BASE . '/include/message/Message.php');
 
 # Only interested in being able to search within the last 30 days.
 $date = date('Y-m-d', strtotime("30 days ago"));
-$msgs = $dbhr->preQuery("SELECT messages.id FROM messages INNER JOIN messages_groups ON messages_groups.msgid = messages.id AND messages_groups.collection = 'Approved' WHERE messages.id NOT IN (SELECT msgid FROM messages_index) AND messages_groups.deleted = 0 AND messages_groups.arrival >= ? ORDER BY messages_groups.arrival DESC;", [
+$msgs = $dbhr->preQuery("SELECT msgid FROM messages_groups WHERE messages_groups.collection = 'Approved' AND messages_groups.msgid NOT IN (SELECT msgid FROM messages_index) AND messages_groups.deleted = 0 AND messages_groups.arrival >= ? ORDER BY messages_groups.arrival DESC;", [
     $date
 ]);
 
+$count = 0;
+
 foreach ($msgs as $msg) {
-    $m = new Message($dbhr, $dbhm, $msg['id']);
-    error_log("#{$msg['id']} " . $m->getPrivate('arrival') . " " . $m->getSubject());
+    $m = new Message($dbhr, $dbhm, $msg['msgid']);
+    error_log("#{$msg['msgid']} " . $m->getPrivate('arrival') . " " . $m->getSubject());
     $m->index();
+    $count++;
+    error_log("$count / " . count($msgs));
 }
