0aef01b614eb62765e658c44e828f43555760a3d
Edward Hibbert <edward@ehibbert.org.uk>
2018-01-21 12:42:24 +0000

    Some of the authorities we have polygons for use MULTIPOLYGON which causes problems; use largest of the POLYGONs in those instead

--- scripts/fix/fix_authorities.php
+++ scripts/fix/fix_authorities.php
@@ -1,27 +1,49 @@
 <?php
 define('SQLLOG', FALSE);
 
+# Some authority data is MULTIPOLYGON.  This causes problems with some of the MySQL geometry functions - probably
+# bugs.  So find such multipolygons and use the largest of its multipolygons instead.
 require_once dirname(__FILE__) . '/../../include/config.php';
 require_once(IZNIK_BASE . '/include/db.php');
 require_once(IZNIK_BASE . '/include/utils.php');
+require_once(IZNIK_BASE . '/lib/geoPHP/geoPHP.inc');
 
-$auths = $dbhr->preQuery("SELECT id, name, COALESCE(simplified, polygon) AS poly, AsText(COALESCE(simplified, polygon)) AS polytext FROM authorities WHERE id = ?;", [ 72890 ]);
+$i = 0;
+
+$auths = $dbhr->preQuery("SELECT id, AsText(polygon) AS polytext FROM authorities WHERE LOCATE('MULTIPOLYGON', AsText(polygon)) > 0;");
 
 foreach ($auths as $auth) {
-    error_log("Auth {$auth['id']}");
-    $groups = $dbhr->preQuery("SELECT id, GeomFromText(COALESCE(poly, polyofficial)) AS poly, COALESCE(poly, polyofficial) AS polytext FROM groups WHERE type = 'Freegle' AND publish = 1 AND onhere = 1;");
+    $i++;
 
-    foreach ($groups as $group) {
-        error_log("...group {$group['id']}");
-        try {
-            $simps = $dbhr->preQuery("SELECT ST_Intersection(?, ?);", [
-                $group['poly'],
-                $auth['poly']
-            ]);
-            error_log("{$auth['id']} {$group['id']} worked");
-        } catch (Exception $e) {
-            error_log("{$auth['id']} {$auth['polytext']} vs {$group['id']} {$group['polytext']} failed");
-            exit(0);
+    if ($i % 100 == 0) {
+        error_log($i);
+    }
+
+    $maxarea = 0;
+    $poly = NULL;
+
+    if (strpos($auth['polytext'], 'MULTIPOLYGON') !== FALSE) {
+        error_log("Auth {$auth['id']}");
+        $p = geoPHP::load($auth['polytext'], 'wkt');
+        $comps = $p->getComponents();
+
+        foreach ($comps as $comp) {
+            if ($comp->area() > $maxarea) {
+                $maxarea = $comp->area();
+                $poly = $comp;
+            }
+        }
+
+        if ($poly) {
+            try {
+                $dbhm->preExec("UPDATE authorities SET polygon = GeomFromText(?), simplified = ST_Simplify(GeomFromText(?), 0.001) WHERE id = ?;", [
+                    $poly->asText(),
+                    $poly->asText(),
+                    $auth['id']
+                ]);
+            } catch (Exception $e) {
+                error_log("Failed " . $e->getMessage() . " {$auth['id']}");
+            }
         }
     }
 }
