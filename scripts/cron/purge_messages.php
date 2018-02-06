137103fd4d272263006226cb912eed2d1d00e37b
Edward Hibbert <edward@ehibbert.org.uk>
2018-02-06 11:41:01 +0000

    Improve message purging

--- scripts/cron/purge_messages.php
+++ scripts/cron/purge_messages.php
@@ -11,220 +11,227 @@ require_once(IZNIK_BASE . '/include/message/MessageCollection.php');
 
 $lockh = lockScript(basename(__FILE__));
 
-# Bypass our usual DB class as we don't want the overhead nor to log.
-$dsn = "mysql:host={$dbconfig['host']};dbname=iznik;charset=utf8";
-$dbhmold = $dbhm;
+try {
+    # Bypass our usual DB class as we don't want the overhead nor to log.
+    $dsn = "mysql:host={$dbconfig['host']};dbname=iznik;charset=utf8";
+    $dbhmold = $dbhm;
 
-$dbhm = new PDO($dsn, $dbconfig['user'], $dbconfig['pass'], array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => FALSE
-));
+    $dbhm = new PDO($dsn, $dbconfig['user'], $dbconfig['pass'], array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => FALSE
+    ));
 
-$dsn = "mysql:host={$dbconfig['host']};dbname=information_schema;charset=utf8";
+    $dsn = "mysql:host={$dbconfig['host']};dbname=information_schema;charset=utf8";
 
-$dbhschema = new PDO($dsn, $dbconfig['user'], $dbconfig['pass'], array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => FALSE
-));
+    $dbhschema = new PDO($dsn, $dbconfig['user'], $dbconfig['pass'], array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => FALSE
+    ));
 
-$sql = "SELECT * FROM KEY_COLUMN_USAGE WHERE REFERENCED_TABLE_NAME = 'messages' AND table_schema = '" . SQLDB . "';";
-$schema = $dbhschema->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    $sql = "SELECT * FROM KEY_COLUMN_USAGE WHERE REFERENCED_TABLE_NAME = 'messages' AND table_schema = '" . SQLDB . "';";
+    $schema = $dbhschema->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-# Purge Yahoo notify messages
-$start = date('Y-m-d', strtotime("midnight 2 days ago"));
-error_log("Purge Yahoo notify messages before $start");
-$total = 0;
+    # Purge Yahoo notify messages
+    $start = date('Y-m-d', strtotime("midnight 2 days ago"));
+    error_log("Purge Yahoo notify messages before $start");
+    $total = 0;
 
-$m = new Message($dbhmold, $dbhmold);
+    $m = new Message($dbhmold, $dbhmold);
 
-do {
-    $sql = "SELECT messages.id FROM messages WHERE fromaddr = 'notify@yahoogroups.com' AND date <= '$start' LIMIT 1000;";
-    $msgs = $dbhm->query($sql)->fetchAll();
-    foreach ($msgs as $msg) {
-        #error_log($msg['id']);
-        $m->quickDelete($schema, $msg['id']);
-        $total++;
+    do {
+        $sql = "SELECT messages.id FROM messages WHERE fromaddr = 'notify@yahoogroups.com' AND date <= '$start' LIMIT 1000;";
+        $msgs = $dbhm->query($sql)->fetchAll();
+        foreach ($msgs as $msg) {
+            #error_log($msg['id']);
+            $m->quickDelete($schema, $msg['id']);
+            $total++;
 
-        if ($total % 10 == 0) {
-            error_log("...$total");
+            if ($total % 10 == 0) {
+                error_log("...$total");
+            }
         }
-    }
-} while (count($msgs) > 0);
+    } while (count($msgs) > 0);
 
-# Purge old messages_history - it's only used for spam checking so we don't need to keep it indefinitely.
-$start = date('Y-m-d', strtotime("midnight 31 days ago"));
-error_log("Purge messages_history before $start");
+    # Purge old messages_history - it's only used for spam checking so we don't need to keep it indefinitely.
+    $start = date('Y-m-d', strtotime("midnight 31 days ago"));
+    error_log("Purge messages_history before $start");
 
-$total = 0;
-do {
-    $sql = "SELECT id FROM messages_history WHERE arrival < '$start' LIMIT 1000;";
-    $msgs = $dbhm->query($sql)->fetchAll();
-    foreach ($msgs as $msg) {
-        $dbhm->exec("DELETE FROM messages_history WHERE id = {$msg['id']};");
-        $total++;
+    $total = 0;
+    do {
+        $sql = "SELECT id FROM messages_history WHERE arrival < '$start' LIMIT 1000;";
+        $msgs = $dbhm->query($sql)->fetchAll();
+        foreach ($msgs as $msg) {
+            $dbhm->exec("DELETE FROM messages_history WHERE id = {$msg['id']};");
+            $total++;
 
-        if ($total % 1000 == 0) {
-            error_log("...$total");
+            if ($total % 1000 == 0) {
+                error_log("...$total");
+            }
         }
-    }
-} while (count($msgs) > 0);
+    } while (count($msgs) > 0);
 
-error_log("Deleted $total");
+    error_log("Deleted $total");
 
-# Purge messages which have been in Pending or Queued for ages.  Probably the group isn't being sync'd properly
-$start = date('Y-m-d', strtotime("midnight 31 days ago"));
-error_log("Purge pending / queued before $start");
+    # Purge messages which have been in Pending or Queued for ages.  Probably the group isn't being sync'd properly
+    $start = date('Y-m-d', strtotime("midnight 31 days ago"));
+    error_log("Purge pending / queued before $start");
 
-$total = 0;
-do {
-    $sql = "SELECT msgid FROM messages_groups WHERE collection IN ('" . MessageCollection::PENDING . "', '" . MessageCollection::QUEUED_YAHOO_USER . "') AND arrival < '$start' LIMIT 1000;";
-    $msgs = $dbhm->query($sql)->fetchAll();
-    foreach ($msgs as $msg) {
-        $dbhm->exec("DELETE FROM messages WHERE id = {$msg['msgid']};");
-        $total++;
+    $total = 0;
+    do {
+        $sql = "SELECT msgid FROM messages_groups WHERE collection IN ('" . MessageCollection::PENDING . "', '" . MessageCollection::QUEUED_YAHOO_USER . "') AND arrival < '$start' LIMIT 1000;";
+        $msgs = $dbhm->query($sql)->fetchAll();
+        foreach ($msgs as $msg) {
+            $dbhm->exec("DELETE FROM messages WHERE id = {$msg['msgid']};");
+            $total++;
 
-        if ($total % 1000 == 0) {
-            error_log("...$total");
+            if ($total % 1000 == 0) {
+                error_log("...$total");
+            }
         }
-    }
-} while (count($msgs) > 0);
+    } while (count($msgs) > 0);
 
-error_log("Deleted $total");
+    error_log("Deleted $total");
 
-# Purge messages which have been stuck waiting for Yahoo users for ages.
-$start = date('Y-m-d', strtotime("midnight 31 days ago"));
-error_log("Purge waiting for Yahoo before $start");
+    # Purge messages which have been stuck waiting for Yahoo users for ages.
+    $start = date('Y-m-d', strtotime("midnight 31 days ago"));
+    error_log("Purge waiting for Yahoo before $start");
 
-$total = 0;
-do {
-    $sql = "SELECT msgid FROM messages_groups WHERE collection = '" . MessageCollection::QUEUED_YAHOO_USER . "' AND arrival < '$start' LIMIT 1000;";
-    $msgs = $dbhm->query($sql)->fetchAll();
-    foreach ($msgs as $msg) {
-        $dbhm->exec("DELETE FROM messages WHERE id = {$msg['msgid']};");
-        $total++;
+    $total = 0;
+    do {
+        $sql = "SELECT msgid FROM messages_groups WHERE collection = '" . MessageCollection::QUEUED_YAHOO_USER . "' AND arrival < '$start' LIMIT 1000;";
+        $msgs = $dbhm->query($sql)->fetchAll();
+        foreach ($msgs as $msg) {
+            $dbhm->exec("DELETE FROM messages WHERE id = {$msg['msgid']};");
+            $total++;
 
-        if ($total % 1000 == 0) {
-            error_log("...$total");
+            if ($total % 1000 == 0) {
+                error_log("...$total");
+            }
         }
-    }
-} while (count($msgs) > 0);
+    } while (count($msgs) > 0);
 
-error_log("Deleted $total");
+    error_log("Deleted $total");
 
-# Purge old drafts.
-$start = date('Y-m-d', strtotime("midnight 31 days ago"));
-error_log("Purge old drafts before $start");
+    # Purge old drafts.
+    $start = date('Y-m-d', strtotime("midnight 31 days ago"));
+    error_log("Purge old drafts before $start");
 
-$total = 0;
-do {
-    $sql = "SELECT msgid FROM messages_drafts WHERE timestamp < '$start' LIMIT 1000;";
-    $msgs = $dbhm->query($sql)->fetchAll();
-    foreach ($msgs as $msg) {
-        $dbhm->exec("DELETE FROM messages WHERE id = {$msg['msgid']};");
-        $total++;
+    $total = 0;
+    do {
+        $sql = "SELECT msgid FROM messages_drafts WHERE timestamp < '$start' LIMIT 1000;";
+        $msgs = $dbhm->query($sql)->fetchAll();
+        foreach ($msgs as $msg) {
+            $dbhm->exec("DELETE FROM messages WHERE id = {$msg['msgid']};");
+            $total++;
 
-        if ($total % 1000 == 0) {
-            error_log("...$total");
+            if ($total % 1000 == 0) {
+                error_log("...$total");
+            }
         }
-    }
-} while (count($msgs) > 0);
+    } while (count($msgs) > 0);
 
-error_log("Deleted $total");
+    error_log("Deleted $total");
 
-# Purge old non-Freegle messages
-$start = date('Y-m-d', strtotime("midnight 31 days ago"));
-error_log("Purge non-Freegle before $start");
+    # Purge old non-Freegle messages
+    $start = date('Y-m-d', strtotime("midnight 31 days ago"));
+    error_log("Purge non-Freegle before $start");
 
-$total = 0;
-do {
-    $sql = "SELECT msgid FROM messages_groups INNER JOIN groups ON messages_groups.groupid = groups.id WHERE `arrival` <= '$start' AND groups.type != 'Freegle' LIMIT 1000;";
-    $msgs = $dbhm->query($sql)->fetchAll();
-    foreach ($msgs as $msg) {
-        $dbhm->exec("DELETE FROM messages WHERE id = {$msg['msgid']};");
-        $total++;
+    $total = 0;
+    do {
+        $sql = "SELECT msgid FROM messages_groups INNER JOIN groups ON messages_groups.groupid = groups.id WHERE `arrival` <= '$start' AND groups.type != 'Freegle' LIMIT 1000;";
+        $msgs = $dbhm->query($sql)->fetchAll();
+        foreach ($msgs as $msg) {
+            $dbhm->exec("DELETE FROM messages WHERE id = {$msg['msgid']};");
+            $total++;
 
-        if ($total % 1000 == 0) {
-            error_log("...$total");
+            if ($total % 1000 == 0) {
+                error_log("...$total");
+            }
         }
-    }
-} while (count($msgs) > 0);
+    } while (count($msgs) > 0);
 
-error_log("Deleted $total");
+    error_log("Deleted $total");
 
-# Now purge messages which have been deleted - we keep them for a while for PD purposes.
-$start = date('Y-m-d', strtotime("midnight 2 days ago"));
-$end = date('Y-m-d', strtotime("midnight 31 days ago"));
-error_log("Purge deleted messages before $start");
-$total = 0;
+    # Now purge messages which have been deleted - we keep them for a while for PD purposes.
+    $start = date('Y-m-d', strtotime("midnight 2 days ago"));
+    $end = date('Y-m-d', strtotime("midnight 31 days ago"));
+    error_log("Purge deleted messages before $start");
+    $total = 0;
 
-do {
-    $sql = "SELECT messages.id FROM messages WHERE date >= '$end' AND deleted IS NOT NULL AND deleted <= '$start' LIMIT 1000;";
-    $msgs = $dbhm->query($sql)->fetchAll();
-    foreach ($msgs as $msg) {
-        $dbhm->exec("DELETE FROM messages WHERE id = {$msg['id']};");
-        $total++;
+    do {
+        $sql = "SELECT messages.id FROM messages WHERE date >= '$end' AND deleted IS NOT NULL AND deleted <= '$start' LIMIT 1000;";
+        $msgs = $dbhm->query($sql)->fetchAll();
+        foreach ($msgs as $msg) {
+            $dbhm->exec("DELETE FROM messages WHERE id = {$msg['id']};");
+            $total++;
 
-        if ($total % 1000 == 0) {
-            error_log("...$total");
+            if ($total % 1000 == 0) {
+                error_log("...$total");
+            }
         }
-    }
-} while (count($msgs) > 0);
+    } while (count($msgs) > 0);
 
-# We don't need the HTML content or full message for old messages - we're primarily interested in the text body, and
-# these are large attributes.
-$start = date('Y-m-d', strtotime("midnight 2 days ago"));
-$end = date('Y-m-d', strtotime("midnight 31 days ago"));
-error_log("Purge HTML body for messages before $start");
-$total = 0;
-$id = NULL;
+    # We don't need the HTML content or full message for old messages - we're primarily interested in the text body, and
+    # these are large attributes.
+    $start = date('Y-m-d', strtotime("midnight 2 days ago"));
+    $end = date('Y-m-d', strtotime("midnight 31 days ago"));
+    error_log("Purge HTML body for messages before $start");
+    $total = 0;
+    $id = NULL;
 
-do {
-    $sql = "SELECT id FROM messages WHERE arrival >= '$end' AND arrival <= '$start' AND htmlbody IS NOT NULL;";
-    $msgs = $dbhr->preQuery($sql);
-    error_log("Found " . count($msgs));
-    foreach ($msgs as $msg) {
-        $sql = "UPDATE messages SET htmlbody = NULL WHERE id = {$msg['id']};";
-        $count = $dbhm->exec($sql);
-        $total += $count;
-        error_log("...$id = $total");
-    }
-} while (count($msgs) > 0);
-
-error_log("Purge message for messages before $start");
-$start = date('Y-m-d', strtotime("midnight 30 days ago"));
-$end = date('Y-m-d', strtotime("midnight 60 days ago"));
-$total = 0;
-$id = NULL;
-
-do {
-    $sql = "SELECT id FROM messages WHERE arrival >= '$end' AND arrival <= '$start' AND message IS NOT NULL;";
-    $msgs = $dbhr->preQuery($sql);
-    foreach ($msgs as $msg) {
-        $sql = "UPDATE messages SET message = NULL WHERE id = {$msg['id']};";
-        $count = $dbhm->exec($sql);
-        $total += $count;
-        error_log("...$id = $total");
-    }
-} while (count($msgs) > 0);
-
-# Now purge messages which are stranded, not on any groups and not referenced from any chats or drafts.
-$start = date('Y-m-d', strtotime("midnight 2 days ago"));
-error_log("Purge stranded messages before $start");
-$total = 0;
-
-do {
-    $sql = "SELECT messages.id FROM messages WHERE arrival <= '$start' AND id NOT IN (SELECT DISTINCT msgid FROM messages_groups) AND id NOT IN (SELECT DISTINCT refmsgid FROM chat_messages) AND id NOT IN (SELECT DISTINCT msgid FROM messages_drafts) LIMIT 1000;";
-    $msgs = $dbhm->query($sql)->fetchAll();
-    foreach ($msgs as $msg) {
-        $dbhm->exec("DELETE FROM messages WHERE id = {$msg['id']};");
-        $total++;
-
-        if ($total % 1000 == 0) {
-            error_log("...$total");
+    do {
+        $sql = "SELECT id FROM messages WHERE arrival >= '$end' AND arrival <= '$start' AND htmlbody IS NOT NULL LIMIT 1000;";
+        $msgs = $dbhr->preQuery($sql);
+        error_log("Found " . count($msgs));
+        foreach ($msgs as $msg) {
+            $sql = "UPDATE messages SET htmlbody = NULL WHERE id = {$msg['id']};";
+            $count = $dbhm->exec($sql);
+            $total += $count;
+            if ($total % 1000 == 0) {
+                error_log("...$total");
+            }
         }
-    }
-} while (count($msgs) > 0);
+    } while (count($msgs) > 0);
 
-error_log("Deleted $total");
+    error_log("Purge message for messages before $start");
+    $start = date('Y-m-d', strtotime("midnight 30 days ago"));
+    $end = date('Y-m-d', strtotime("midnight 60 days ago"));
+    $total = 0;
+    $id = NULL;
+
+    do {
+        $sql = "SELECT id FROM messages WHERE arrival >= '$end' AND arrival <= '$start' AND message IS NOT NULL;";
+        $msgs = $dbhr->preQuery($sql);
+        foreach ($msgs as $msg) {
+            $sql = "UPDATE messages SET message = NULL WHERE id = {$msg['id']};";
+            $count = $dbhm->exec($sql);
+            $total += $count;
+            error_log("...$id = $total");
+        }
+    } while (count($msgs) > 0);
+
+    # Now purge messages which are stranded, not on any groups and not referenced from any chats or drafts.
+    $start = date('Y-m-d', strtotime("midnight 2 days ago"));
+    error_log("Purge stranded messages before $start");
+    $total = 0;
+
+    do {
+        $sql = "SELECT messages.id FROM messages WHERE arrival <= '$start' AND id NOT IN (SELECT DISTINCT msgid FROM messages_groups) AND id NOT IN (SELECT DISTINCT refmsgid FROM chat_messages) AND id NOT IN (SELECT DISTINCT msgid FROM messages_drafts) LIMIT 1000;";
+        $msgs = $dbhm->query($sql)->fetchAll();
+        foreach ($msgs as $msg) {
+            $dbhm->exec("DELETE FROM messages WHERE id = {$msg['id']};");
+            $total++;
+
+            if ($total % 1000 == 0) {
+                error_log("...$total");
+            }
+        }
+    } while (count($msgs) > 0);
+
+    error_log("Deleted $total");
+} catch (Exception $e) {
+    error_log("Failed with " . $e->getMessage());
+    exit(1);
+}
 
 unlockScript($lockh);
