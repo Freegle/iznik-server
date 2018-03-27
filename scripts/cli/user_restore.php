73458c1dbc9b17bd14c62886255ab2c5502c7f2f
Edward Hibbert <edward@ehibbert.org.uk>
2018-03-27 15:39:42 +0100

    More work on user_restore

--- scripts/cli/user_restore.php
+++ scripts/cli/user_restore.php
@@ -34,7 +34,7 @@ if (count($opts) < 1) {
     if (!$luid) {
         error_log("...create");
         $luid = $ulive->create(NULL, NULL, NULL);
-        $u->addEmail($email);
+        $ulive->addEmail($email);
     }
 
     $uback = new User($dbhback, $dbhback);
@@ -46,7 +46,11 @@ if (count($opts) < 1) {
         # User attributes
         foreach (['fullname', 'firstname', 'lastname', 'yahooUserId', 'yahooid', 'systemrole'] as $att) {
             $val = $uback->getPrivate($att);
-            $ulive->setPrivate($att, $val);
+            try {
+                $ulive->setPrivate($att, $val);
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+            }
         }
 
         # Tables with foreign keys.
@@ -71,17 +75,59 @@ if (count($opts) < 1) {
             'logs_sql' => [ 'userid' ]
                  ] as $table => $keys) {
             foreach ($keys as $key) {
-                #error_log("Table $table key $key");
+                error_log("Table $table key $key");
                 $rows = $dbhback->preQuery("SELECT * FROM $table WHERE $key = ?;", [ $buid ]);
 
                 foreach ($rows as $row) {
+                    if ($table == 'users_emails') {
+                        # This is generated so we must not provide it
+                        unset($row['md5hash']);
+                    }
+
+                    if ($table == 'chat_messages' || $table == 'chat_roster') {
+                        # The roomid may be different.
+                        $roomid = $row['chatid'];
+                        $oldrooms = $dbhback->preQuery("SELECT * FROM chat_rooms WHERE id = ?;", [
+                            $roomid
+                        ]);
+
+                        foreach ($oldrooms as $oldroom) {
+                            error_log("Old chat {$oldroom['chattype']}, {$oldroom['groupid']}, {$oldroom['user1']}, {$oldroom['user2']}");
+                            $sql = "SELECT * FROM chat_rooms WHERE chattype = '{$oldroom['chattype']}' ";
+
+                            if (pres('groupid', $oldroom)) {
+                                $sql .= "AND groupid = {$oldroom['groupid']} ";
+                            }
+
+                            if (pres('user1', $oldroom)) {
+                                $i = $oldroom['user1'] == $buid ? $luid : $oldroom['user1'];
+                                $sql .= " AND user1 = $i";
+                            }
+
+                            if (pres('user2', $oldroom)) {
+                                $i = $oldroom['user2'] == $buid ? $luid : $oldroom['user2'];
+                                $sql .= " AND user2 = $i";
+                            }
+
+                            error_log("Look for new rooms $sql");
+                            $newrooms = $dbhr->preQuery($sql);
+
+                            foreach ($newrooms as $newroom) {
+                                error_log("Found new room $roomid");
+                                $roomid = $newroom['id'];
+                            }
+                        }
+
+                        $row['chatid'] = $roomid;
+                    }
+
                     error_log("  #{$row['id']}");
 
                     # The row might or might not exist.
-                    unset($row['id']);
+                    #unset($row['id']);
                     $sql1 = "INSERT INTO $table (";
                     $sql2 = ") VALUES (";
-                    $sql3 = ") ON DUPLICATE KEY UPDATE $key = $luid";
+                    $sql3 = ") ON DUPLICATE KEY UPDATE $key = $luid, id = LAST_INSERT_ID(id)";
                     $first = TRUE;
                     $vals = [];
                     $vals2 = [];
@@ -110,6 +156,7 @@ if (count($opts) < 1) {
                     #error_log($sql . var_export($v, TRUE));
                     try {
                         $rc = $dbhm->preExec($sql, $v);
+                        error_log("Inserted " . $dbhm->lastInsertId());
                     } catch (Exception $e) {
                         error_log($e->getMessage());
                     }
