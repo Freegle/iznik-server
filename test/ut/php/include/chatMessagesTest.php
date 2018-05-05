531c386617a592c1cd4cc1d0f9f1259eb781bb92
Edward Hibbert <edward@ehibbert.org.uk>
2018-05-05 07:31:21 +0100

    Test language detection for spam

--- test/ut/php/include/chatMessagesTest.php
+++ test/ut/php/include/chatMessagesTest.php
@@ -334,6 +334,9 @@ class chatMessagesTest extends IznikTestCase {
         assertTrue($m->checkReview("Test\r\nTest email@domain.com\r\n"));
         assertFalse($m->checkReview("Test\r\nTest email@" . USER_DOMAIN . " email\r\n"));
 
+        # French
+        assertTrue($m->checkReview("Suzanne et Joseph étaient nés dans les deux premières années de leur arrivée à la colonie. Après la naissance de Suzanne, la mère abandonna l’enseignement d’état. Elle ne donna plus que des leçons particulières de français. Son mari avait été nommé directeur d’une école indigène et, disaient-elle, ils avaient vécu très largement malgré la charge de leurs enfants. Ces années-là furent sans conteste les meilleures de sa vie, des années de bonheur. Du moins c’étaient ce qu’elle disait. Elle s’en souvenait comme d’une terre lointaine et rêvée, d’une île. Elle en parlait de moins en moins à mesure qu’elle vieillissait, mais quand elle en parlait c’était toujours avec le même acharnement. Alors, à chaque fois, elle découvrait pour eux de nouvelles perfections à cette perfection, une nouvelle qualité à son mari, un nouvel aspect de l’aisance qu’ils connaissaient alors, et qui tendaient à devenir une opulence dont Joseph et Suzanne doutaient un peu."));
+
         error_log(__METHOD__ . " end");
     }
 
