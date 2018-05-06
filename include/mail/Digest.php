60d13676b0d66e7cd3a9dae2322ad47c23624ff6
Edward Hibbert <edward@ehibbert.org.uk>
2018-05-06 18:09:05 +0100

    Retire old single message code

--- include/mail/Digest.php
+++ include/mail/Digest.php
@@ -4,7 +4,6 @@ require_once(IZNIK_BASE . '/include/utils.php');
 require_once(IZNIK_BASE . '/include/message/Message.php');
 require_once(IZNIK_BASE . '/include/misc/Log.php');
 require_once(IZNIK_BASE . '/include/group/Group.php');
-require_once(IZNIK_BASE . '/mailtemplates/digest/single.php');
 require_once(IZNIK_BASE . '/mailtemplates/digest/multiple.php');
 require_once(IZNIK_BASE . '/mailtemplates/digest/message.php');
 require_once(IZNIK_BASE . '/mailtemplates/digest/off.php');
