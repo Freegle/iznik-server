8aba09b7483ac0c7073467e40c8357430f628ca2
Edward Hibbert <edward@ehibbert.org.uk>
2018-08-07 16:39:26 +0100

    Travis - PAF fix

--- install/paf.sql
+++ install/paf.sql
@@ -1,3 +1,3 @@
 INSERT INTO `locations` (`id`, `osm_id`, `name`, `type`, `osm_place`, `geometry`, `ourgeometry`, `gridid`, `postcodeid`, `areaid`, `canon`, `popularity`, `osm_amenity`, `osm_shop`, `maxdimension`, `lat`, `lng`, `timestamp`) VALUES
-  (1687412, '189543628', 'SA65 9ET', 'Line', 0, GeomFromText('POINT(-4.939858, 52.006292)'), NULL, NULL, NULL, NULL, 'sa659et', 0, 0, 0, '0.002916', '52.006292', '-4.939858', '2016-08-23 06:01:25');
+  (1687412, '189543628', 'SA65 9ET', 'Line', 0, GeomFromText('POINT(-4.939858 52.006292)'), NULL, NULL, NULL, NULL, 'sa659et', 0, 0, 0, '0.002916', '52.006292', '-4.939858', '2016-08-23 06:01:25');
 INSERT INTO `paf_addresses` (`id`, `postcodeid`) VALUES   (102367696, 1687412);
