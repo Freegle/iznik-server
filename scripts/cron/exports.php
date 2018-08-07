457ccf1e73d21ec43e11aacf4cefc2c4e44e5980
Edward Hibbert <edward@ehibbert.org.uk>
2018-08-07 13:26:41 +0100

    Travis - exports

--- scripts/cron/exports.php
+++ scripts/cron/exports.php
@@ -8,25 +8,27 @@ $lockh = lockScript(basename(__FILE__));
 
 error_log("Start exports script");
 
-try {
-    error_log("Look for export");
-    $exports = $dbhr->preQuery("SELECT * FROM users_exports WHERE completed IS NULL ORDER BY id ASC;");
+do {
+    try {
+        error_log("Look for export");
+        $exports = $dbhr->preQuery("SELECT * FROM users_exports WHERE completed IS NULL ORDER BY id ASC;");
 
-    foreach ($exports as $export) {
-        error_log("Do export {$export['id']} for {$export['userid']} requested {$export['requested']}");
-        $u = new User($dbhr, $dbhm, $export['userid']);
-        $u->export($export['id'], $export['tag']);
-        error_log("...done");
+        foreach ($exports as $export) {
+            error_log("Do export {$export['id']} for {$export['userid']} requested {$export['requested']}");
+            $u = new User($dbhr, $dbhm, $export['userid']);
+            $u->export($export['id'], $export['tag']);
+            error_log("...done");
+        }
+
+        # Zap data for old exports.
+        $mysqltime = date("Y-m-d H:i:s", strtotime("midnight 7 days ago"));
+        $dbhm->preExec("UPDATE users_exports SET data = NULL WHERE completed IS NOT NULL AND completed < '$mysqltime';");
+    } catch (Exception $e) {
+        error_log("Top-level exception " . $e->getMessage() . "\n");
     }
 
-    # Zap data for old exports.
-    $mysqltime = date("Y-m-d H:i:s", strtotime("midnight 7 days ago"));
-    $dbhm->preExec("UPDATE users_exports SET data = NULL WHERE completed IS NOT NULL AND completed < '$mysqltime';");
-
     sleep(30);
-} catch (Exception $e) {
-    error_log("Top-level exception " . $e->getMessage() . "\n");
-}
+} while (true);
 
 error_log("End export script");
 
