2c9f0581d76bbd1009f57e105e728c73692d88db
Edward Hibbert <edward@ehibbert.org.uk>
2018-10-13 14:55:46 +0100

    Reply count missing in summary

--- include/message/MessageCollection.php
+++ include/message/MessageCollection.php
@@ -142,7 +142,7 @@ class MessageCollection
                 $summjoin = $summary ? ", messages_groups.msgtype AS type, messages.fromuser, messages.subject,
                 (SELECT publishconsent FROM users WHERE users.id = messages.fromuser) AS publishconsent, 
                 (SELECT groupid FROM messages_groups WHERE msgid = messages.id) AS groupid,
-                (SELECT COUNT(DISTINCT userid) FROM chat_messages WHERE refmsgid = messages.id AND reviewrejected = 0 AND reviewrequired = 0 AND chat_messages.userid != messages.fromuser AND chat_messages.type = '{ChatMessage::TYPE_INTERESTED}') AS replycount,                  
+                (SELECT COUNT(DISTINCT userid) FROM chat_messages WHERE refmsgid = messages.id AND reviewrejected = 0 AND reviewrequired = 0 AND chat_messages.userid != messages.fromuser AND chat_messages.type = 'Interested') AS replycount,                  
                 (SELECT messages_attachments.id FROM messages_attachments WHERE msgid = messages.id ORDER BY messages_attachments.id LIMIT 1) AS attachmentid, 
                 (SELECT messages_outcomes.id FROM messages_outcomes WHERE msgid = messages.id ORDER BY id DESC LIMIT 1) AS outcomeid": '';
 
