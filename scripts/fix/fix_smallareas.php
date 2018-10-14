46df246e6297ff516d403ec98e8942eab39f4e7b
Edward Hibbert <edward@ehibbert.org.uk>
2018-10-14 15:10:03 +0100

    Fix small location areas

--- scripts/fix/fix_smallareas.php
+++ scripts/fix/fix_smallareas.php
@@ -7,7 +7,7 @@ require_once(IZNIK_BASE . '/include/misc/Location.php');
 
 $opts = getopt('g:');
 
-$groups = $dbhr->preQuery("SELECT id, region, nameshort FROM groups WHERE type = 'Freegle' AND publish = 1 AND onhere = 1 AND nameshort LIKE '%{$opts['g']}%' ORDER BY LOWER(nameshort) ASC;");
+$groups = $dbhr->preQuery("SELECT id, region, nameshort FROM groups WHERE type = 'Freegle' AND publish = 1 AND onhere = 1 AND nameshort LIKE '%{$opts['g']}%' AND lower(nameshort) >= 'northdown-ardspeninsula_greencycle' ORDER BY LOWER(nameshort) ASC;");
 $l = new Location($dbhr, $dbhm);
 
 foreach ($groups as $group) {
@@ -18,39 +18,43 @@ foreach ($groups as $group) {
     $count = 0;
 
     foreach ($locs as $loc) {
-        $isarea = $dbhr->preQuery("SELECT COUNT(*) AS count FROM locations WHERE areaid = ?;", [
-            $loc['id']
-        ]);
-
-        if ($isarea[0]['count'] > 0) {
-            $areas = $dbhr->preQuery("SELECT ST_AREA(COALESCE(ourgeometry, geometry)) AS area, COALESCE(ourgeometry, geometry) AS geom FROM locations LEFT JOIN locations_excluded ON locations.areaid = locations_excluded.locationid WHERE id = ? AND locations_excluded.locationid IS NULL;", [
+        try {
+            $isarea = $dbhr->preQuery("SELECT COUNT(*) AS count FROM locations WHERE areaid = ?;", [
                 $loc['id']
-            ], FALSE, FALSE);
-            foreach ($areas as $area) {
-                #error_log("...#{$loc['id']} {$loc['name']}, area {$area['area']}");
+            ]);
 
-                if ($area['area'] < 0.001) {
-                    # Small area.  Is it inside another one?
-                    $overlaps = $dbhr->preQuery("SELECT locations_spatial.locationid, ST_AREA(geometry) AS area FROM locations_spatial LEFT JOIN locations_excluded ON locations_excluded.locationid = locations_spatial.locationid WHERE ST_Contains(geometry, ?) AND locations_spatial.locationid != ? AND locations_excluded.locationid IS NULL HAVING area < 0.001 AND area > 0.00001 ORDER BY ST_AREA(geometry) ASC LIMIT 1;", [
-                        $area['geom'],
-                        $loc['id']
-                    ], FALSE, FALSE);
+            if ($isarea[0]['count'] > 0) {
+                $areas = $dbhr->preQuery("SELECT ST_AREA(COALESCE(ourgeometry, geometry)) AS area, COALESCE(ourgeometry, geometry) AS geom FROM locations LEFT JOIN locations_excluded ON locations.areaid = locations_excluded.locationid WHERE id = ? AND locations_excluded.locationid IS NULL;", [
+                    $loc['id']
+                ], FALSE, FALSE);
+                foreach ($areas as $area) {
+                    #error_log("...#{$loc['id']} {$loc['name']}, area {$area['area']}");
 
-                    foreach ($overlaps as $overlap) {
-                        $overlocs = $dbhr->preQuery("SELECT id, name FROM locations WHERE id = ?;", [
-                            $overlap['locationid']
+                    if ($area['area'] < 0.001) {
+                        # Small area.  Is it inside another one?
+                        $overlaps = $dbhr->preQuery("SELECT locations_spatial.locationid, ST_AREA(geometry) AS area FROM locations_spatial LEFT JOIN locations_excluded ON locations_excluded.locationid = locations_spatial.locationid WHERE ST_Contains(geometry, ?) AND locations_spatial.locationid != ? AND locations_excluded.locationid IS NULL HAVING area < 0.001 AND area > 0.00001 ORDER BY ST_AREA(geometry) ASC LIMIT 1;", [
+                            $area['geom'],
+                            $loc['id']
                         ], FALSE, FALSE);
 
-                        foreach ($overlocs as $overloc) {
-                            error_log("...hide #{$loc['id']} {$loc['name']}, area {$area['area']} as overlapped by #{$overloc['id']} {$overloc['name']}, area {$overlap['area']}");
-                            $dbhm->preExec("INSERT IGNORE INTO locations_excluded (locationid, groupid) VALUES (?, ?);", [
-                                $loc['id'],
-                                $group['id']
-                            ]);
+                        foreach ($overlaps as $overlap) {
+                            $overlocs = $dbhr->preQuery("SELECT id, name FROM locations WHERE id = ?;", [
+                                $overlap['locationid']
+                            ], FALSE, FALSE);
+
+                            foreach ($overlocs as $overloc) {
+                                error_log("...hide #{$loc['id']} {$loc['name']}, area {$area['area']} as overlapped by #{$overloc['id']} {$overloc['name']}, area {$overlap['area']}");
+                                $dbhm->preExec("INSERT IGNORE INTO locations_excluded (locationid, groupid) VALUES (?, ?);", [
+                                    $loc['id'],
+                                    $group['id']
+                                ]);
+                            }
                         }
                     }
                 }
             }
+        } catch (Exception $e) {
+            error_log("...skip {$loc['id']} " . $e->getMessage());
         }
 
         $count++;
