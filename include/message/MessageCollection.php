c9f708b03721fce5c0011d6e7db9dc9fb7deb903
Edward Hibbert <edward@ehibbert.org.uk>
2018-10-23 16:55:28 +0100

    Push Notifications using wrong host

--- include/message/MessageCollection.php
+++ include/message/MessageCollection.php
@@ -158,7 +158,7 @@ class MessageCollection
                 if ($userids) {
                     # We only query on a small set of userids, so it's more efficient to get the list of messages from them
                     # first.
-                    $seltab = "(SELECT id, arrival, " . ($summary ? 'subject, ' : '') . "fromuser, deleted, `type` FROM messages WHERE fromuser IN (" . implode(',', $userids) . ")) messages";
+                    $seltab = "(SELECT id, arrival, " . ($summary ? 'subject, ' : '') . "fromuser, deleted, `type`, textbody FROM messages WHERE fromuser IN (" . implode(',', $userids) . ")) messages";
                     $sql = "SELECT messages_groups.msgid AS id, messages.arrival, messages_groups.collection $summjoin FROM messages_groups INNER JOIN $seltab ON messages_groups.msgid = messages.id AND messages.deleted IS NULL $outcomeq1 WHERE $dateq $oldest $typeq $groupq $collectionq AND messages_groups.deleted = 0 $outcomeq2 ORDER BY messages_groups.arrival DESC LIMIT $limit";
                 } else if (count($groupids) > 0) {
                     # The messages_groups table has a multi-column index which makes it quick to find the relevant messages.
