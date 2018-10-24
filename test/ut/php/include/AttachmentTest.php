ad2de1696656e0e9ffae17be652dbe23518061dc
edwh <edward@ehibbert.org.uk>
2018-10-24 21:19:29 +0100

    Coverage - attachments

--- test/ut/php/include/AttachmentTest.php
+++ test/ut/php/include/AttachmentTest.php
@@ -44,22 +44,54 @@ class AttachmentTest extends IznikTestCase {
         error_log(__METHOD__ . " end");
     }
 
-    public function testArchive() {
+    private $blobCount = 0;
+
+    public function createBlockBlob() {
+        $this->blobCount++;
+    }
+
+    public function attTypes() {
+        # Most types don't archive.
+        return([
+            [ Attachment::TYPE_MESSAGE, 2 ],
+            [ Attachment::TYPE_GROUP, 0 ],
+            [ Attachment::TYPE_NEWSLETTER, 0 ],
+            [ Attachment::TYPE_COMMUNITY_EVENT, 0 ],
+            [ Attachment::TYPE_VOLUNTEERING , 0],
+            [ Attachment::TYPE_CHAT_MESSAGE, 2 ],
+            [ Attachment::TYPE_USER, 0 ],
+            [ Attachment::TYPE_NEWSFEED, 2 ],
+            [ Attachment::TYPE_STORY, 0 ]
+        ]);
+    }
+
+    /**
+     * @dataProvider attTypes
+     */
+    public function testArchive($attType, $blobCount) {
         error_log(__METHOD__);
 
+        $data = file_get_contents(IZNIK_BASE . '/test/ut/php/images/chair.jpg');
+
+        $a = $this->getMockBuilder('Attachment')
+            ->setConstructorArgs([ $this->dbhr, $this->dbhm, NULL, $attType ])
+            ->setMethods(array('getAzure'))
+            ->getMock();
+        $a->method('getAzure')->willReturn($this);
+
+        $attid = $a->create(NULL, 'image/jpeg', $data);
+        assertNotNull($attid);
+
+        $ret = $a->archive();
+        assertEquals($blobCount, $this->blobCount);
+        assertEquals($blobCount > 0, $ret);
+
         if (!getenv('STANDALONE')) {
-            $data = file_get_contents(IZNIK_BASE . '/test/ut/php/images/chair.jpg');
-            $a = new Attachment($this->dbhr, $this->dbhm);
-            $attid = $a->create(NULL, 'image/jpeg', $data);
-            assertNotNull($attid);
-
-            assertTrue($a->archive());
             $dat2 = $a->getData();
-
             assertEquals($data, $dat2);
         }
 
-        assertTrue(TRUE);
+        $a->delete();
 
         error_log(__METHOD__ . " end");
     }
