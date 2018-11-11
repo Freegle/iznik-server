eac3360d8d2f92c93efd1f825a3ff27ecc5d8223
Edward Hibbert <edward@ehibbert.org.uk>
2018-11-11 10:26:08 +0000

    Add "mentored" field to group

--- http/api/group.php
+++ http/api/group.php
@@ -175,7 +175,7 @@ function group() {
 
                         # Other support-settable attributes
                         if ($me->isAdminOrSupport()) {
-                            foreach (['publish', 'licenserequired', 'lat', 'lng'] as $att) {
+                            foreach (['publish', 'licenserequired', 'lat', 'lng', 'mentored'] as $att) {
                                 $val = presdef($att, $_REQUEST, NULL);
                                 if (array_key_exists($att, $_REQUEST)) {
                                     $g->setPrivate($att, $val);
