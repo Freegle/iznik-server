60fe3aa2197af73080906c30220564a5bf5d2c2f
edwh <edward@ehibbert.org.uk>
2019-01-05 22:34:16 +0000

    API error when logged out

--- include/message/Message.php
+++ include/message/Message.php
@@ -951,7 +951,7 @@ class Message
                 $maxreposts = $repost * $reposts['max'];
                 $expiretime = max($expiretime, $maxreposts);
 
-                if (!$ret['canedit'] && $myid === $this->getFromuser()) {
+                if (!$ret['canedit'] && $myid && $myid === $this->getFromuser()) {
                     # This is our own message, which we may be able to edit if the group allows it.
                     $allowedits = $g->getSetting('allowedits', [ 'moderated' => TRUE, 'group' => TRUE ]);
                     $ourPS = $me->getMembershipAtt($group['groupid'], 'ourPostingStatus');
