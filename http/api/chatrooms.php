cdf6e0cb58740a9b69a2fb6da82409c7ceb433db
Edward Hibbert <edward@ehibbert.org.uk>
2019-02-19 08:30:43 +0000

    UT fixes

--- http/api/chatrooms.php
+++ http/api/chatrooms.php
@@ -27,7 +27,8 @@ function chatrooms() {
                     $ret['chatroom']['unseen'] = $r->unseenCountForUser($myid);
                     $ret['chatroom']['lastmsgseen'] = $r->lastSeenForUser($myid);
 
-                    if (time() - strtotime($ret['chatroom']['latestmessage']) > 31 * 24 * 3600) {
+                    if (!pres('latestmessage', $ret['chatroom']) ||
+                        time() - strtotime($ret['chatroom']['latestmessage']) > 31 * 24 * 3600) {
                         // This is an old chat which we have decided to fetch. Update latestmessage to make
                         // sure it will subequently appear in listForUser
                         $r->ensureAppearInList($id);
