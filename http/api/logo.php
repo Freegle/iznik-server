4d7c7f10d886fb0ed5cb657a792b01d6808d3a03
Edward Hibbert <edward@ehibbert.org.uk>
2019-02-24 14:23:08 +0000

    Add active flag to logos table

--- http/api/logo.php
+++ http/api/logo.php
@@ -8,7 +8,7 @@ function logo() {
         case 'GET': {
             $ret = [ 'ret' => 0, 'status' => 'Success'];
 
-            $logos = $dbhr->preQuery("SELECT * FROM logos WHERE `date` LIKE ? ORDER BY RAND();", [
+            $logos = $dbhr->preQuery("SELECT * FROM logos WHERE `date` LIKE ? AND active = 1 ORDER BY RAND();", [
                 date("m-d")
             ]);
 
