f99a5e456bf49f0771bb3e29003fea1607f8763b
Edward Hibbert <edward@ehibbert.org.uk>
2019-05-09 09:30:48 +0100

    Include Facebook donations in tally for stroll

--- http/api/stroll.php
+++ http/api/stroll.php
@@ -7,12 +7,33 @@ function stroll() {
     switch ($_REQUEST['type']) {
         case 'GET': {
             $route = $dbhr->preQuery("SELECT * FROM stroll_route ORDER BY id ASC");
+
+            # Sponsors include people who've gone via the dedicated page, people who've donated via Facebook (because
+            # that's the only fundraising that's happening on Facebook).
+            #
+            # We're also promoting the stroll on our normal donation requests, but we can't count those because a)
+            # we'd double-count donations via the dedicated page and b) we would get some donations anyway.
             $sponsors = $dbhr->preQuery("SELECT * FROM stroll_sponsors ORDER BY timestamp ASC");
-            $nights = $dbhr->preQuery("SELECT * FROM stroll_nights ORDER BY id ASC");
 
             foreach ($sponsors as &$sponsor) {
                 $sponsor['timestamp'] = ISODate($sponsor['timestamp']);
             }
+
+            $facebooks = $dbhr->preQuery("SELECT * FROM users_donations WHERE source = 'Facebook';");
+
+            foreach ($facebooks as $facebook) {
+                $sponsors[] = [
+                    'name' => 'Anonymous Facebook donation',
+                    'amount' => $facebook['GrossAmount'],
+                    'timestamp' => ISODate($facebook['timestamp'])
+                ];
+            }
+
+            usort($sponsors, function($a, $b) {
+                return(strcmp($a['timestamp'], $b['timestamp']));
+            });
+
+            $nights = $dbhr->preQuery("SELECT * FROM stroll_nights ORDER BY id ASC");
 
             $ret = [
                 'ret' => 0,
@@ -27,7 +48,6 @@ function stroll() {
 
         case 'POST': {
             $sponsorname = presdef('sponsorname', $_REQUEST, NULL);
-            error_log("Sponsor $sponsorname");
 
             if ($sponsorname) {
                 $dbhm->preExec("INSERT INTO stroll_sponsors (name) VALUES (?)", [
