05aa87b25b8b160d701d7654277d85016bf90168
Edward Hibbert <edward@ehibbert.org.uk>
2019-08-15 17:07:45 +0100

    Proxy to serve job ads

--- http/adview.php
+++ http/adview.php
@@ -1,6 +1,8 @@
 <?php
+header('Content-Type: application/json');
 
-$url = "https://adview.online/api/v1/jobs.json?publisher=2053&location=" . $_REQUEST['location'];
+$url = "https://adview.online/api/v1/jobs.json?publisher=2053&limit=50&location=" . urlencode($_REQUEST['location']);
+error_log("Get jobs $url");
 
 $data = file_get_contents($url);
 
