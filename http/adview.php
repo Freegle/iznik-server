92a35420770259464a01433174dad0c6c2208d15
Edward Hibbert <edward@ehibbert.org.uk>
2019-08-16 11:38:38 +0100

    Add IP to adview script

--- http/adview.php
+++ http/adview.php
@@ -1,7 +1,8 @@
 <?php
 header('Content-Type: application/json');
 
-$url = "https://adview.online/api/v1/jobs.json?publisher=2053&limit=50&location=" . urlencode($_REQUEST['location']);
+$ip = presdef('REMOTE_ADDR', $_SERVER, NULL);
+$url = "https://adview.online/api/v1/jobs.json?publisher=2053&limit=50&user_ip=$ip&location=" . urlencode($_REQUEST['location']);
 
 $data = file_get_contents($url);
 
