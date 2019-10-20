f2e61793d0bee75c82c4d12eee2350ff570ea263
Edward Hibbert <edward@ehibbert.org.uk>
2019-10-20 12:13:59 +0100

    Bumping when last active on users migrated from Yahoo

---
+++ scripts/fix/fix_lastactive.php
@@ -0,0 +1,17 @@
+<?php
+# Script to bump lastaccess for members of a group.  Used after importing a group from Yahoo to trigger sending
+# mails to old users.
+require_once dirname(__FILE__) . '/../../include/config.php';
+require_once(IZNIK_BASE . '/include/db.php');
+require_once(IZNIK_BASE . '/include/utils.php');
+
+$opts = getopt('i:');
+
+$gid = $opts['i'];
+
+$membs = $dbhr->preQuery("SELECT userid FROM memberships WHERE groupid = $gid");
+
+foreach ($membs as $memb) {
+    error_log($memb['userid']);
+    $dbhm->preExec("UPDATE users SET lastaccess = NOW() WHERE id = {$memb['userid']}");
+}
