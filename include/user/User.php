f5dc4bdbf5e87e31ab16d93e640e083598fcdd2a
Edward Hibbert <edward@ehibbert.org.uk>
2019-11-01 13:31:04 +0000

    Reposting messages not showing in message history

--- include/user/User.php
+++ include/user/User.php
@@ -2022,7 +2022,7 @@ class User extends Entity
         }
     }
 
-    public function getPublic($groupids = NULL, $history = TRUE, $logs = FALSE, &$ctx = NULL, $comments = TRUE, $memberof = TRUE, $applied = TRUE, $modmailsonly = FALSE, $emailhistory = FALSE, $msgcoll = [MessageCollection::APPROVED], $historyfull = FALSE)
+    public function getPublic($groupids = NULL, $history = TRUE, $logs = FALSE, &$ctx = NULL, $comments = TRUE, $memberof = TRUE, $applied = TRUE, $modmailsonly = FALSE, $emailhistory = FALSE, $msgcoll = NULL, $historyfull = FALSE)
     {
         $atts = [];
 
