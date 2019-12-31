9f5a968a5b0ba96aadbbd89fb541ea35fe14f85b
Edward Hibbert <edward@ehibbert.org.uk>
2019-12-31 15:12:22 +0000

    Fix donations, which are over-estimating.  The messages for this checkin and the previous one are the wrong way round.

--- include/misc/apiheaders.php
+++ include/misc/apiheaders.php
@@ -14,7 +14,7 @@ if (!(($call == 'image' || $call == 'profile') && $type == 'GET')) {
 //$dom = array_key_exists('HTTP_ORIGIN', $_SERVER) ? $_SERVER['HTTP_ORIGIN'] : NULL;
 //$allow = $dom ? "$dom" : '*';
 //@header("Access-Control-Allow-Origin: $allow");
-@header('Access-Control-Allow-Headers: ' . $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
+@header('Access-Control-Allow-Headers: ' . array_key_exists('HTTP_ACCESS_CONTROL_REQUEST_HEADERS', $_SERVER) ? $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'] : "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 @header('Access-Control-Allow-Credentials: true');
 @header('P3P:CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"');
 
