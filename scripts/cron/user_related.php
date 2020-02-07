40c4f9254ebed5668d3937ef1b3647b7ce02d35c
Edward Hibbert <edward@ehibbert.org.uk>
2020-02-07 11:03:16 +0000

    Add name to related mail

--- scripts/cron/user_related.php
+++ scripts/cron/user_related.php
@@ -16,7 +16,7 @@ foreach ($relateds as $related) {
     $u = new User($dbhr, $dbhm, $related['user1']);
     $groups = array_column($u->getMemberships(), 'nameshort');
 
-    $str = "This is an automated mail.  We have detected users who may be the same real person.\n\nThere are several possible reasons for this:\n1) They are confused.  You can merge accounts in ModTools from Members->Approved to help them.\n2) Multiple people are using the same physical device, e.g. in a family.  This is probably fine.\n3) They are trying to pretend to be multiple people.\n\nPlease review their activity on your groups; if you think it's fine, then you don't need to do anything and we won't tell you about them again.  If you need more info, please reply to this mail and Support will help you.\n\nUser #{$related['user1']} (" . $u->getEmailPreferred() . ", on " . implode(', ', $groups) . ") may be the same as:\n";
+    $str = "This is an automated mail.  We have detected users who may be the same real person.\n\nThere are several possible reasons for this:\n1) They are confused.  You can merge accounts in ModTools from Members->Approved to help them.\n2) Multiple people are using the same physical device, e.g. in a family.  This is probably fine.\n3) They are trying to pretend to be multiple people.\n\nPlease review their activity on your groups; if you think it's fine, then you don't need to do anything and we won't tell you about them again.  If you need more info, please reply to this mail and Support will help you.\n\nUser #{$related['user1']} (" . $u->getName() . " " . $u->getEmailPreferred() . ", on " . implode(', ', $groups) . ") may be the same as:\n";
     $others = $dbhr->preQuery("SELECT * FROM users_related WHERE user1 = ?;", [
         $related['user1']
     ]);
@@ -24,7 +24,7 @@ foreach ($relateds as $related) {
     foreach ($others as $other) {
         $u2 = new User($dbhr, $dbhm, $other['user2']);
         $groups = array_column($u2->getMemberships(), 'nameshort');
-        $str .= "...#{$other['user2']} " . $u2->getEmailPreferred()  . ", on " . implode(', ', $groups);
+        $str .= "...#{$other['user2']} " . $u2->getName() . " " . $u2->getEmailPreferred()  . ", on " . implode(', ', $groups);
 
         if (!$other['notified']) {
             $str .= " - just discovered";
