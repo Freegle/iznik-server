0ca9363fbecc099301dd59730d5f6069b188c911
Edward Hibbert <edward@ehibbert.org.uk>
2020-02-07 18:13:55 +0000

    More on related users

--- scripts/cron/user_related.php
+++ scripts/cron/user_related.php
@@ -15,8 +15,18 @@ $relateds = $dbhr->preQuery("SELECT * FROM users_related WHERE notified = 0 AND 
 foreach ($relateds as $related) {
     $u = new User($dbhr, $dbhm, $related['user1']);
     $groups = array_column($u->getMemberships(), 'nameshort');
+    $email = $u->getEmailPreferred();
+    $email = $email ? $email : 'email not known';
 
-    $str = "This is an automated mail.  We have detected users who may be the same real person.\n\nThere are several possible reasons for this:\n1) They are confused.  You can merge accounts in ModTools from Members->Approved to help them.\n2) Multiple people are using the same physical device, e.g. in a family.  This is probably fine.\n3) They are trying to pretend to be multiple people.\n\nPlease review their activity on your groups; if you think it's fine, then you don't need to do anything and we won't tell you about them again.  If you need more info, please reply to this mail and Support will help you.\n\nUser #{$related['user1']} (" . $u->getName() . " " . $u->getEmailPreferred() . ", on " . implode(', ', $groups) . ") may be the same as:\n";
+    $str = "This is an automated mail.  We have detected users who may be the same real person.\n\n" +
+        "There are several possible reasons for this:\n" +
+        "1) They are confused.  You can merge accounts in ModTools from Members->Approved to help them.\n" +
+        "2) Multiple people are using the same physical device, e.g. in a family.  This is probably fine.\n" +
+        "3) They are trying to pretend to be multiple people.\n\n" +
+        "Please review their activity on your groups; if you think it's fine, then you don't need to do anything and we won't tell you about them again.\n\n" +
+        "Note that some of these emails might not be members of your group.  That would be common if they've just signed in using the wrong method.\n\n" +
+        "If you need more info, please reply to this mail and Support will help you.\n\n" +
+        "User #{$related['user1']} (" . $u->getName() . " $email, on " . implode(', ', $groups) . ") may be the same as:\n";
     $others = $dbhr->preQuery("SELECT * FROM users_related WHERE user1 = ?;", [
         $related['user1']
     ]);
@@ -24,7 +34,9 @@ foreach ($relateds as $related) {
     foreach ($others as $other) {
         $u2 = new User($dbhr, $dbhm, $other['user2']);
         $groups = array_column($u2->getMemberships(), 'nameshort');
-        $str .= "...#{$other['user2']} " . $u2->getName() . " " . $u2->getEmailPreferred()  . ", on " . implode(', ', $groups);
+        $email = $u2->getEmailPreferred();
+        $email = $email ? $email : 'email not known';
+        $str .= "...#{$other['user2']} " . $u2->getName() . " $email, on " . implode(', ', $groups);
 
         if (!$other['notified']) {
             $str .= " - just discovered";
