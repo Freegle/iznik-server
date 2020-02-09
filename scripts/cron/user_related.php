c1c855c62e9f1ea482d8fb1efafab73500f768ee
edwh <edward@ehibbert.org.uk>
2020-02-09 15:21:59 +0000

    Don't mail about deleted related users

--- scripts/cron/user_related.php
+++ scripts/cron/user_related.php
@@ -14,46 +14,66 @@ $relateds = $dbhr->preQuery("SELECT * FROM users_related WHERE notified = 0 AND 
 
 foreach ($relateds as $related) {
     $u = new User($dbhr, $dbhm, $related['user1']);
-    $groups = array_column($u->getMemberships(), 'nameshort');
-    $email = $u->getEmailPreferred();
-    $email = $email ? $email : 'email not known';
 
-    $str = "This is an automated mail.  We have detected users who may be the same real person.\n\n" .
-        "There are several possible reasons for this:\n" .
-        "1) They are confused.  You can merge accounts in ModTools from Members->Approved to help them.\n" .
-        "2) Multiple people are using the same physical device, e.g. in a family.  This is probably fine.\n" .
-        "3) They are trying to pretend to be multiple people.\n\n" .
-        "Please review their activity on your groups; if you think it's fine, then you don't need to do anything and we won't tell you about them again.\n\n" .
-        "Note that some of these emails might not be members of your group.  That would be common if they've just signed in using the wrong method.\n\n" .
-        "If you need more info, please reply to this mail and Support will help you.\n\n" .
-        "User #{$related['user1']} (" . $u->getName() . " $email, on " . implode(', ', $groups) . ") may be the same as:\n";
-    $others = $dbhr->preQuery("SELECT * FROM users_related WHERE user1 = ?;", [
-        $related['user1']
-    ]);
-
-    $anemail = FALSE;
-
-    foreach ($others as $other) {
-        $u2 = new User($dbhr, $dbhm, $other['user2']);
-        $groups = array_column($u2->getMemberships(), 'nameshort');
-        $email = $u2->getEmailPreferred();
-
-        if ($email) {
-            $anemail = TRUE;
-        }
-
+    if (!$u->getPrivate('deleted')) {
+        $u1membs = $u->getMemberships();
+        $groups = array_column($u1membs, 'nameshort');
+        $email = $u->getEmailPreferred();
         $email = $email ? $email : 'email not known';
-        $str .= "...#{$other['user2']} " . $u2->getName() . " $email, on " . implode(', ', $groups);
 
-        if (!$other['notified']) {
-            $str .= " - just discovered";
+        $str = "User #{$related['user1']} (" . $u->getName() . " $email, on " . implode(', ', $groups) . ") may be the same as:\n";
+        $others = $dbhr->preQuery("SELECT * FROM users_related WHERE user1 = ?;", [
+            $related['user1']
+        ]);
+
+        $anemail = FALSE;
+
+        foreach ($others as $other) {
+            $u2 = new User($dbhr, $dbhm, $other['user2']);
+
+            if (!$u2->getPrivate('deleted')) {
+                $u2membs = $u2->getMemberships();
+                $groups = array_column($u2membs, 'nameshort');
+                $email = $u2->getEmailPreferred();
+
+                if ($email) {
+                    $anemail = TRUE;
+                }
+
+                $email = $email ? $email : 'email not known';
+                $str .= "...#{$other['user2']} " . $u2->getName() . " $email, on " . implode(', ', $groups);
+
+                if (!$other['notified']) {
+                    $str .= " - just discovered";
+                }
+
+                $str .= "\n";
+            }
         }
 
-        $str .= "\n";
-    }
+        $common = array_intersect(array_column($u1membs, 'id'), array_column($u2membs, 'id'));
 
-    # Can only really notify if there is an email address for them to check.
-    if ($anemail) {
+        # Can only really notify local groups if there is an email address for them to check.
+        if (count($common) && $anemail) {
+            $g = new Group($dbhr, $dbhm, $common[0]);
+            mail($g->getModsEmail() . ", log@ehibbert.org.uk", "Possible related users {$related['user1']}",
+                "This is an automated mail.  We have detected users who may be the same real person.\n\n" .
+                "There are several possible reasons for this:\n" .
+                "1) They are confused.  You can merge accounts in ModTools from Members->Approved to help them.\n" .
+                "2) Multiple people are using the same physical device, e.g. in a family.  This is probably fine.\n" .
+                "3) They are trying to pretend to be multiple people.\n\n" .
+                "Please review their activity on your groups; if you think it's fine, then you don't need to do anything and we won't tell you about them again.\n\n" .
+                "Note that some of these emails might not be members of your group.  That would be common if they've just signed in using the wrong method.\n\n" .
+                "If you need more info, please reply to this mail and Support will help you.  You can see more background at https://discourse.ilovefreegle.org/t/identifying-related-freeglers\n\n" .
+                $str,
+                [], '-f' . SUPPORT_ADDR);
+        } else {
+            mail(SUPPORT_ADDR . ", log@ehibbert.org.uk", "Possible related users {$related['user1']}",
+                "We've mailed support about this either because there is no email for one of the users, or there are no groups in common.  That means local volunteers couldn't contact or merge them.\n\n" .
+                $str,
+                [], '-f' . SUPPORT_ADDR);
+        }
+
         $dbhm->preExec("UPDATE users_related SET notified = 1 WHERE user1 = ? AND user2 = ?;", [
             $related['user1'],
             $related['user2']
@@ -63,7 +83,5 @@ foreach ($relateds as $related) {
             $related['user2'],
             $related['user1']
         ]);
-
-        mail("log@ehibbert.org.uk", "Possible related users {$related['user1']}", $str, [], '-f' . SUPPORT_ADDR);
     }
 }
