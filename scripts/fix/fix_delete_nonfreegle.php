0e645fac4299c7878632b81695dc61e07d9c187f
Edward Hibbert <edward@ehibbert.org.uk>
2020-02-16 13:11:23 +0000

    Delete non-Freegle groups from the platform now that Yahoo is dead.

---
+++ scripts/fix/fix_delete_nonfreegle.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../include/config.php';
+require_once(IZNIK_BASE . '/include/db.php');
+require_once(IZNIK_BASE . '/include/utils.php');
+require_once(IZNIK_BASE . '/include/group/Group.php');
+
+$groups = $dbhr->preQuery("SELECT id FROM groups WHERE type = 'Other';");
+
+foreach ($groups as $group) {
+    $g = Group::get($dbhr, $dbhm, $group['id']);
+    error_log($g->getName());
+    $g->delete();
+}
