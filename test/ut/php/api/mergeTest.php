51b37679db6db62b152bafa08a895c5b7d57b782
Edward Hibbert <edward@ehibbert.org.uk>
2020-03-03 14:32:18 +0000

    More WIP on merge requests

---
+++ test/ut/php/api/mergeTest.php
@@ -0,0 +1,147 @@
+<?php
+
+if (!defined('UT_DIR')) {
+    define('UT_DIR', dirname(__FILE__) . '/../..');
+}
+require_once UT_DIR . '/IznikAPITestCase.php';
+require_once IZNIK_BASE . '/include/user/User.php';
+require_once IZNIK_BASE . '/include/group/Group.php';
+require_once IZNIK_BASE . '/include/mail/MailRouter.php';
+require_once IZNIK_BASE . '/include/message/MessageCollection.php';
+
+/**
+ * @backupGlobals disabled
+ * @backupStaticAttributes disabled
+ */
+class userAPITest extends IznikAPITestCase {
+    public $dbhr, $dbhm;
+
+    protected function setUp() {
+        parent::setUp ();
+
+        /** @var LoggedPDO $dbhr */
+        /** @var LoggedPDO $dbhm */
+        global $dbhr, $dbhm;
+        $this->dbhr = $dbhr;
+        $this->dbhm = $dbhm;
+
+        $dbhm->preExec("DELETE FROM users WHERE fullname = 'Test User';");
+        $dbhm->preExec("DELETE FROM groups WHERE nameshort = 'testgroup';");
+        $dbhm->preExec("DELETE FROM users WHERE yahooUserId = '1';");
+
+        # Create a moderator and log in as them
+        $this->group = Group::get($this->dbhr, $this->dbhm);
+        $this->groupid = $this->group->create('testgroup', Group::GROUP_REUSE);
+
+        $u = User::get($this->dbhr, $this->dbhm);
+        $this->uid = $u->create(NULL, NULL, 'Test User');
+        $this->user = User::get($this->dbhr, $this->dbhm, $this->uid);
+        $this->user->addEmail('test@test.com');
+        assertEquals(1, $this->user->addMembership($this->groupid));
+        assertGreaterThan(0, $this->user->addLogin(User::LOGIN_NATIVE, NULL, 'testpw'));
+
+        $this->uid2 = $u->create(NULL, NULL, 'Test User');
+        $this->user2 = User::get($this->dbhr, $this->dbhm, $this->uid2);
+        $this->user2->addEmail('test2@test.com');
+        assertEquals(1, $this->user2->addMembership($this->groupid));
+        assertGreaterThan(0, $this->user2->addLogin(User::LOGIN_NATIVE, NULL, 'testpw'));
+
+        assertTrue($this->user->login('testpw'));
+
+        $this->plugin = new Plugin($this->dbhr, $this->dbhm);
+    }
+
+    public function testMerge() {
+        $u1 = User::get($this->dbhm, $this->dbhm);
+        $id1 = $u1->create('Test', 'User', NULL);
+        $u1->addMembership($this->groupid);
+        $u2 = User::get($this->dbhm, $this->dbhm);
+        $id2 = $u2->create('Test', 'User', NULL);
+        $u2->addMembership($this->groupid);
+        $u2->addEmail('test2@test.com', 0);
+        $u3 = User::get($this->dbhm, $this->dbhm);
+        $id3 = $u3->create('Test', 'User', NULL);
+        $u3->addEmail('test3@test.com', 0);
+        $u3->addMembership($this->groupid);
+        $u4 = User::get($this->dbhm, $this->dbhm);
+
+        $id4 = $u4->create('Test', 'User', NULL);
+        $u4->addMembership($this->groupid, User::ROLE_MODERATOR);
+        $u4->addEmail('test4@test.com', 0);
+        assertGreaterThan(0, $u4->addLogin(User::LOGIN_NATIVE, NULL, 'testpw'));
+        assertTrue($u4->login('testpw'));
+
+        # Request a merge as a mod.
+        $ret = $this->call('merge', 'PUT', [
+            'user1' => $id1,
+            'user2' => $id2,
+            'email' => FALSE // For UT
+        ]);
+        assertEquals(0, $ret['ret']);
+        $mid = $ret['id'];
+        $uid = $ret['uid'];
+
+        assertNotNull($mid);
+        assertNotNull($uid);
+
+        # Log out.
+        $_SESSION['id'] = NULL;
+        $_SESSION['logged_in'] = FALSE;
+
+        # Accept with invalid info
+        $ret = $this->call('merge', 'POST', [
+            'user1' => $id1,
+            'user2' => $id2,
+            'id' => $mid,
+            'uid' => 'zzz',
+            'action' => 'Accept'
+        ]);
+
+        assertEquals(2, $ret['ret']);
+
+        $ret = $this->call('merge', 'POST', [
+            'user1' => $id1,
+            'user2' => $id2,
+            'id' => -1,
+            'uid' => $uid,
+            'action' => 'Accept'
+        ]);
+
+        assertEquals(2, $ret['ret']);
+
+        # Reject
+        $ret = $this->call('merge', 'POST', [
+            'user1' => $id1,
+            'user2' => $id2,
+            'id' => $mid,
+            'uid' => $uid,
+            'action' => 'Reject'
+        ]);
+
+        assertEquals(0, $ret['ret']);
+
+        # Shouldn't be merged.
+        $u1 = new User($this->dbhr, $this->dbhm, $id1);
+        assertEquals($id1, $u1->getId());
+        $u2 = new User($this->dbhr, $this->dbhm, $id2);
+        assertEquals($id2, $u2->getId());
+
+        # Accept
+        $ret = $this->call('merge', 'POST', [
+            'user1' => $id2,
+            'user2' => $id1,
+            'id' => $mid,
+            'uid' => $uid,
+            'action' => 'Accept'
+        ]);
+
+        assertEquals(0, $ret['ret']);
+
+        # Should be merged.
+        $u1 = new User($this->dbhr, $this->dbhm, $id1);
+        assertNull($u1->getId());
+        $u2 = new User($this->dbhr, $this->dbhm, $id2);
+        assertEquals($id2, $u2->getId());
+    }
+}
+
