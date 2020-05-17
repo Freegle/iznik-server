8bd374cc7919c42950ff54291d6efc128abc136a
Edward Hibbert <edward@ehibbert.org.uk>
2020-05-17 09:32:25 +0100

    Re-enable check for mail backlog.

--- scripts/cron/status.php
+++ scripts/cron/status.php
@@ -143,20 +143,19 @@ function status()
 
     if (!$overallwarning) {
         # Check whether we have a backlog sending digests.  This is less important than other warnings.
-        # Disable for COVID as we're not sending.
-//        error_log("Check mail backlogs");
-//        $sql = "SELECT groupid, frequency, backlog FROM (SELECT DISTINCT TIMESTAMPDIFF(HOUR, started, NOW()) AS backlog, groups_digests.* FROM `groups_digests` INNER JOIN groups ON groups.id = groups_digests.groupid WHERE type = 'Freegle' AND onhere = 1 AND publish = 1 HAVING backlog > frequency * 1.5 AND frequency > 0 AND backlog > 0) t
-//ORDER BY backlog DESC LIMIT 1;";
-//        $backlogs = $dbhr->preQuery($sql);
-//
-//        foreach ($backlogs as $backlog) {
-//            $sql = "SELECT count(DISTINCT groupid) AS count FROM (SELECT DISTINCT TIMESTAMPDIFF(HOUR, started, NOW()) AS backlog, groups_digests.* FROM `groups_digests` INNER JOIN groups ON groups.id = groups_digests.groupid WHERE type = 'Freegle' AND onhere = 1 AND publish = 1 HAVING backlog > frequency * 1.5 AND frequency > 0 AND backlog > 0) t;";
-//            $counts = $dbhr->preQuery($sql);
-//            $overallwarning = TRUE;
-//            $info["Mailer"]['warning'] = TRUE;
-//            $info['Mailer']['warningtext'] = "Backlog sending group mails; worst example is {$backlog['backlog']} hours, should be sent every {$backlog['frequency']} hours.  {$counts[0]['count']} groups affected.";
-//            error_log($info['Mailer']['warningtext']);
-//        }
+        error_log("Check mail backlogs");
+        $sql = "SELECT groupid, frequency, backlog FROM (SELECT DISTINCT TIMESTAMPDIFF(HOUR, started, NOW()) AS backlog, groups_digests.* FROM `groups_digests` INNER JOIN groups ON groups.id = groups_digests.groupid WHERE type = 'Freegle' AND onhere = 1 AND publish = 1 HAVING backlog > frequency * 1.5 AND frequency > 0 AND backlog > 0) t
+ORDER BY backlog DESC LIMIT 1;";
+        $backlogs = $dbhr->preQuery($sql);
+
+        foreach ($backlogs as $backlog) {
+            $sql = "SELECT count(DISTINCT groupid) AS count FROM (SELECT DISTINCT TIMESTAMPDIFF(HOUR, started, NOW()) AS backlog, groups_digests.* FROM `groups_digests` INNER JOIN groups ON groups.id = groups_digests.groupid WHERE type = 'Freegle' AND onhere = 1 AND publish = 1 HAVING backlog > frequency * 1.5 AND frequency > 0 AND backlog > 0) t;";
+            $counts = $dbhr->preQuery($sql);
+            $overallwarning = TRUE;
+            $info["Mailer"]['warning'] = TRUE;
+            $info['Mailer']['warningtext'] = "Backlog sending group mails; worst example is {$backlog['backlog']} hours, should be sent every {$backlog['frequency']} hours.  {$counts[0]['count']} groups affected.";
+            error_log($info['Mailer']['warningtext']);
+        }
     }
 
     $ret = [
