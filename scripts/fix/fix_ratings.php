912d2a1b5fd8dd89d7d5542c94e2f624064404a2
Edward Hibbert <edward@ehibbert.org.uk>
2020-06-06 12:56:08 +0100

    Fix ratings

---
+++ scripts/fix/fix_ratings.php
@@ -0,0 +1,20 @@
+<?php
+
+# Run on backup server to recover a user from a backup to the live system.  Use with astonishing levels of caution.
+require_once dirname(__FILE__) . '/../../include/config.php';
+require_once(IZNIK_BASE . '/include/db.php');
+require_once(IZNIK_BASE . '/include/utils.php');
+require_once(IZNIK_BASE . '/include/group/Volunteering.php');
+
+$dsn = "mysql:host=localhost;port=3309;dbname=iznik;charset=utf8";
+$dbhback = new LoggedPDO($dsn, SQLUSER, SQLPASSWORD, array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => FALSE
+));
+
+    $vols = $dbhback->preQuery("SELECT * FROM ratings;");
+
+    foreach ($vols as $vol) {
+        error_log("UPDATE ratings SET timestamp = '{$vol['timestamp']}' WHERE id = {$vol['id']};");
+    }
+
