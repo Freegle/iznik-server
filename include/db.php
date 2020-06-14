c7239f02eaaef0b301ad3e0fce2633343e6ec8c8
Edward Hibbert <edward@ehibbert.org.uk>
2020-06-14 14:33:58 +0100

    Speed-ups: use PDO emulation of prepared statements, which is better for performance

--- include/db.php
+++ include/db.php
@@ -818,16 +818,22 @@ class LoggedPDO {
 # Don't use persistent connections as they don't play nice - PDO can use a connection which was already
 # closed.  It's possible that our retrying would handle this ok, but we should only rely on that if
 # we've tested it better and we need the perf gain.
+#
+# We emulate prepared statements on the client.  This is because we use prepared statements primarily for
+# SQL injection, and we don't reuse the statements much.  Prepared statements involve additional round trips to
+# the SQL server, and therefore they are actually significantly slower.  Emulation is as secure as long as
+# you use a suitable charset (e.g. utf8).  See https://phpdelusions.net/pdo#emulation, which refers to
+# https://stackoverflow.com/questions/134099/are-pdo-prepared-statements-sufficient-to-prevent-sql-injection/12202218
 $dsn = "mysql:host={$dbconfig['host']};port={$dbconfig['port_read']};dbname={$dbconfig['database']};charset=utf8";
 
 $dbhr = new LoggedPDO($dsn, $dbconfig['user'], $dbconfig['pass'], array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => FALSE
+    PDO::ATTR_EMULATE_PREPARES => TRUE
 ), TRUE);
 
 $dsn = "mysql:host={$dbconfig['host']};port={$dbconfig['port_mod']};dbname={$dbconfig['database']};charset=utf8";
 
 $dbhm = new LoggedPDO($dsn, $dbconfig['user'], $dbconfig['pass'], array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => FALSE
+    PDO::ATTR_EMULATE_PREPARES => TRUE
 ), FALSE, $dbhr);
