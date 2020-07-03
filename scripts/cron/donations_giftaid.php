7ab29e8e72ea7f8cb4170dc43d751353af798885
Edward Hibbert <edward@ehibbert.org.uk>
2020-07-03 15:01:00 +0100

    Asking for giftaid multiple times

--- scripts/cron/donations_giftaid.php
+++ scripts/cron/donations_giftaid.php
@@ -16,60 +16,72 @@ $donations = $dbhr->preQuery("SELECT users_donations.* FROM `users_donations` LE
 $sentto = [];
 
 foreach ($donations as $donation) {
-    # Don't send duplicates.
+    # Don't send duplicates, either from previous ones or within this cycle.
     if (!pres($donation['userid'], $sentto)) {
-        $sentto[$donation['userid']] = TRUE;
+        $previous = $dbhr->preQuery("SELECT * FROM users_donations WHERE userid = ? and giftaidchaseup IS NOT NULL;", [
+            $donation['userid']
+        ]);
 
-        # Add a notification onsite.
-        $n = new Notifications($dbhr, $dbhm);
-        $n->add(NULL, $donation['userid'], Notifications::TYPE_GIFTAID, NULL);
-
-        # Mail them
-        try {
-            $u = new User($dbhr, $dbhm, $donation['userid']);
-            $email = $u->getEmailPreferred();
-            error_log("...$email");
-
-            $loader = new Twig_Loader_Filesystem(IZNIK_BASE . '/mailtemplates/twig/donations');
-            $twig = new Twig_Environment($loader);
-            list ($transport, $mailer) = getMailer();
-
-            $date = date(' d-M-Y', strtotime($donation['timestamp']));
-
-            $message = Swift_Message::newInstance()
-                ->setSubject("Could Freegle collect Gift Aid on your donation?")
-                ->setFrom(PAYPAL_THANKS_FROM)
-                ->setReplyTo(PAYPAL_THANKS_FROM)
-                ->setTo($email)
-                ->setBody("You kindly donated to Freegle on $date.  We can make your kind donation go even further if we can claim Gift Aid.  If you can, please go to https://www.ilovefreegle.org/giftaid to complete a donation.");
-
-            Mail::addHeaders($message, Mail::THANK_DONATION);
-
-            $html = $twig->render('chaseup.html', [
-                'name' => $u->getName(),
-                'email' => $u->getEmailPreferred(),
-                'unsubscribe' => $u->loginLink(USER_SITE, $u->getId(), "/unsubscribe", NULL),
-                'date' => $date
+        if (count($previous)) {
+            # Fix up previous bug.
+            $dbhm->preExec("UPDATE users_donations SET giftaidchaseup = NOW() WHERE userid = ?;", [
+                $donation['userid']
             ]);
+        } else {
+            # Never sent one.
+            $sentto[$donation['userid']] = TRUE;
 
-            # Add HTML in base-64 as default quoted-printable encoding leads to problems on
-            # Outlook.
-            $htmlPart = Swift_MimePart::newInstance();
-            $htmlPart->setCharset('utf-8');
-            $htmlPart->setEncoder(new Swift_Mime_ContentEncoder_Base64ContentEncoder);
-            $htmlPart->setContentType('text/html');
-            $htmlPart->setBody($html);
-            $message->attach($htmlPart);
+            # Add a notification onsite.
+            $n = new Notifications($dbhr, $dbhm);
+            $n->add(NULL, $donation['userid'], Notifications::TYPE_GIFTAID, NULL);
 
-            Mail::addHeaders($message, Mail::THANK_DONATION, $u->getId());
+            # Mail them
+            try {
+                $u = new User($dbhr, $dbhm, $donation['userid']);
+                $email = $u->getEmailPreferred();
+                error_log("...$email");
 
-            $mailer->send($message);
+                $loader = new Twig_Loader_Filesystem(IZNIK_BASE . '/mailtemplates/twig/donations');
+                $twig = new Twig_Environment($loader);
+                list ($transport, $mailer) = getMailer();
 
-            # Record the ask.
-            $dbhm->preExec("UPDATE users_donations SET giftaidchaseup = NOW() WHERE id = ?;", [
-                $donation['id']
-            ]);
-        } catch (Exception $e) { error_log("Failed " . $e->getMessage()); };
+                $date = date(' d-M-Y', strtotime($donation['timestamp']));
+
+                $message = Swift_Message::newInstance()
+                    ->setSubject("Could Freegle collect Gift Aid on your donation?")
+                    ->setFrom(PAYPAL_THANKS_FROM)
+                    ->setReplyTo(PAYPAL_THANKS_FROM)
+                    ->setTo($email)
+                    ->setBody("You kindly donated to Freegle on $date.  We can make your kind donation go even further if we can claim Gift Aid.  If you can, please go to https://www.ilovefreegle.org/giftaid to complete a donation.");
+
+                Mail::addHeaders($message, Mail::THANK_DONATION);
+
+                $html = $twig->render('chaseup.html', [
+                    'name' => $u->getName(),
+                    'email' => $u->getEmailPreferred(),
+                    'unsubscribe' => $u->loginLink(USER_SITE, $u->getId(), "/unsubscribe", NULL),
+                    'date' => $date
+                ]);
+
+                # Add HTML in base-64 as default quoted-printable encoding leads to problems on
+                # Outlook.
+                $htmlPart = Swift_MimePart::newInstance();
+                $htmlPart->setCharset('utf-8');
+                $htmlPart->setEncoder(new Swift_Mime_ContentEncoder_Base64ContentEncoder);
+                $htmlPart->setContentType('text/html');
+                $htmlPart->setBody($html);
+                $message->attach($htmlPart);
+
+                Mail::addHeaders($message, Mail::THANK_DONATION, $u->getId());
+
+                $mailer->send($message);
+
+                # Record the ask, for all donations from this user.
+                $dbhm->preExec("UPDATE users_donations SET giftaidchaseup = NOW() WHERE userid = ?;", [
+                    $donation['userid']
+                ]);
+            } catch (Exception $e) { error_log("Failed " . $e->getMessage()); };
+        }
     }
 }
 
