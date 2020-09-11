f8bcd885f1e8a898fee430f080c9925f4a039378
Edward Hibbert <edward@ehibbert.org.uk>
2020-09-11 08:47:40 +0100

    Return lat/lng so we can map TN messages

--- include/message/Message.php
+++ include/message/Message.php
@@ -530,7 +530,8 @@ class Message
 
     public $moderatorAtts = [
         'source', 'sourceheader', 'envelopefrom', 'envelopeto', 'messageid', 'tnpostid',
-        'fromip', 'fromcountry', 'message', 'spamreason', 'spamtype', 'replyto', 'editedby', 'editedat', 'locationid'
+        'fromip', 'fromcountry', 'message', 'spamreason', 'spamtype', 'replyto', 'editedby', 'editedat', 'locationid',
+        'lat', 'lng'
     ];
 
     public $ownerAtts = [
