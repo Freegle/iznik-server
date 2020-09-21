1f4ee3cdfb45016b2c6a71033105935f60bd4dd3
Edward Hibbert <edward@ehibbert.org.uk>
2020-09-21 13:52:16 +0100

    Can't promise

--- include/message/MessageCollection.php
+++ include/message/MessageCollection.php
@@ -193,6 +193,7 @@ class MessageCollection
                 # We might be looking for posts with no outcome.
                 $outcomeq1 = $hasoutcome !== NULL ? " LEFT JOIN messages_outcomes ON messages_outcomes.id = messages.id " : '';
                 $outcomeq2 = $hasoutcome !== NULL ? " HAVING outcomeid IS NULL " : '';
+                $outcomeq3 = $hasoutcome !== NULL ? ", messages_outcomes.id AS outcomeid" : '';
 
                 # We might be getting a summary, in which case we want to get lots of information in the same query
                 # for performance reasons.
@@ -218,7 +219,7 @@ class MessageCollection
                     # We only query on a small set of userids, so it's more efficient to get the list of messages from them
                     # first.
                     $seltab = "(SELECT id, arrival, lat, lng, " . ($summary ? 'subject, ' : '') . "fromuser, deleted, `type`, textbody, source FROM messages WHERE fromuser IN (" . implode(',', $userids) . ")) messages";
-                    $sql = "SELECT 0 AS isdraft, messages_groups.msgid AS id, messages.lat, messages.lng, messages_groups.groupid, messages_groups.arrival, messages_groups.collection, messages_outcomes.id AS outcomeid $summjoin FROM messages_groups INNER JOIN $seltab ON messages_groups.msgid = messages.id AND messages.deleted IS NULL $outcomeq1 WHERE $dateq $oldest $typeq $groupq $collectionq AND messages_groups.deleted = 0 AND messages.fromuser IS NOT NULL $outcomeq2 ORDER BY messages_groups.arrival DESC, messages_groups.msgid DESC LIMIT $limit";
+                    $sql = "SELECT 0 AS isdraft, messages_groups.msgid AS id, messages.lat, messages.lng, messages_groups.groupid, messages_groups.arrival, messages_groups.collection $outcomeq3 $summjoin FROM messages_groups INNER JOIN $seltab ON messages_groups.msgid = messages.id AND messages.deleted IS NULL $outcomeq1 WHERE $dateq $oldest $typeq $groupq $collectionq AND messages_groups.deleted = 0 AND messages.fromuser IS NOT NULL $outcomeq2 ORDER BY messages_groups.arrival DESC, messages_groups.msgid DESC LIMIT $limit";
                 } else if (count($groupids) > 0) {
                     # The messages_groups table has a multi-column index which makes it quick to find the relevant messages.
                     $typeq = $types ? (" AND `msgtype` IN (" . implode(',', $types) . ") ") : '';
