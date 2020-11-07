fd7555adb698a83d828c6d72a17ed3d215702d1c
Edward Hibbert <edward@ehibbert.org.uk>
2020-11-07 08:55:34 +0000

    UT fixes

--- install/schema.sql
+++ install/schema.sql
@@ -2080,13 +2080,11 @@ CREATE TABLE `messages_outcomes` (
   `comments` text COLLATE utf8mb4_unicode_ci,
   `reviewed` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `userid` (`userid`),
   KEY `msgid` (`msgid`),
   KEY `timestamp` (`timestamp`),
   KEY `timestamp_2` (`timestamp`,`outcome`),
   KEY `timestamp_3` (`reviewed`,`timestamp`,`happiness`) USING BTREE,
   CONSTRAINT `messages_outcomes_ibfk_1` FOREIGN KEY (`msgid`) REFERENCES `messages` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `messages_outcomes_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4944183 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
