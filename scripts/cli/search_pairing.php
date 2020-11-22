670fdebb70253d65d9b0e25af2ecce1370dc9ddb
Edward Hibbert <edward@ehibbert.org.uk>
2020-11-22 11:35:18 +0000

    Initial attempt to group related items using magic

---
+++ scripts/cli/search_pairing.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Freegle\Iznik;
+
+# Using https://github.com/nicolasdugue/DirectedLouvain.
+
+define('BASE_DIR', dirname(__FILE__) . '/../..');
+require_once(BASE_DIR . '/include/config.php');
+require_once(IZNIK_BASE . '/include/db.php');
+global $dbhr, $dbhm;
+
+$opts = getopt('m:i:o:w:');
+
+$mode = $opts['m'];
+
+if ($mode == 'export') {
+    $terms = $dbhr->preQuery("SELECT GREATEST(searchterm1, searchterm2) AS t1, LEAST(searchterm1, searchterm2) as t2, COUNT(*) AS count FROM microactions WHERE searchterm1 IS NOT NULL AND version = 2 GROUP BY t1, t2 ORDER BY count DESC;");
+    $renumber = [];
+    $nh = fopen($opts['o'], 'w');
+    $wh = fopen($opts['w'], 'w');
+
+    foreach ($terms as $term) {
+        $t1 = array_search($term['t1'], $renumber, TRUE);
+
+        if ($t1 === FALSE) {
+            $renumber[] = $term['t1'];
+            $t1 = count($renumber);
+        }
+
+        $t2 = array_search($term['t2'], $renumber, TRUE);
+
+        if ($t2 === FALSE) {
+            $renumber[] = $term['t2'];
+            $t2 = count($renumber);
+        }
+
+        fputs($nh, "$t1 $t2 {$term['count']}\n");
+    }
+
+    for ($i = 0; $i < count($renumber); $i++) {
+        fputs($wh, "{$renumber[$i]} $i\n");
+    }
+} else {
+    $fh = fopen($opts['w'],'r');
+    $renumber = [];
+
+    while ($fields = fgetcsv($fh, 0, ' ')){
+        $renumber[$fields[1]] = $fields[0];
+        #error_log("Renumbered {$fields[1]} = {$fields[0]}");
+    }
+
+    $fh = fopen($opts['i'],'r');
+    $communities = [];
+
+    while ($fields = fgetcsv($fh, 0, ' ')){
+        #error_log("Renumbered {$fields[0]} comm {$fields[1]}");
+        if (array_key_exists($fields[0], $renumber)) {
+            $terms = $dbhr->preQuery("SELECT * FROM search_terms WHERE id = ?;", [
+                $renumber[$fields[0] . '']
+            ]);
+
+            foreach ($terms as $term) {
+                $communities[$fields[1]][] = $term['term'];
+            }
+        }
+    }
+
+    foreach ($communities as $community) {
+        foreach ($community as $term) {
+            echo $term . ", ";
+        }
+
+        echo "\n";
+    }
+}
