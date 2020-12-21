852d17df6bfb37691c12666856514e79bbd4eac7
Edward Hibbert <edward@ehibbert.org.uk>
2020-12-21 21:23:45 +0000

    Fix warning

--- scripts/cron/check_spammers.php
+++ scripts/cron/check_spammers.php
@@ -6,12 +6,9 @@ define('BASE_DIR', dirname(__FILE__) . '/../..');
 require_once(BASE_DIR . '/include/config.php');
 
 require_once(IZNIK_BASE . '/include/db.php');
-global $dbhr, $dbhm, $dbconfig;
+global $dbhr, $dbhm;
 
 $lockh = Utils::lockScript(basename(__FILE__));
-
-$dsn = "mysql:host={$dbconfig['host']};dbname=iznik;charset=utf8";
-$at = 0;
 
 $s = new Spam($dbhr, $dbhm);
 $spammers = $s->removeSpamMembers();
