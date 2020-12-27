35c9d9c74c0c71cdcffc6b3e57d266045f242987
Edward Hibbert <edward@ehibbert.org.uk>
2020-12-27 12:02:28 +0000

    Remove dead code

--- scripts/cli/group_moderate_covid.php
+++ scripts/cli/group_moderate_covid.php
@@ -16,50 +16,6 @@ $id = $opts['i'];
 
 $a = new Authority($dbhr, $dbhm, $id);
 
-//# Check for weird polygons.
-//$groups = $dbhr->preQuery("SELECT id, nameshort FROM groups WHERE type = ? AND publish = 1 AND onmap = 1;", [
-//    Group::GROUP_FREEGLE
-//]);
-//
-//foreach ($groups as $group) {
-//    try {
-//        $sql = "SELECT groups.id, nameshort, namefull, lat, lng,
-//       CASE WHEN poly IS NOT NULL THEN poly ELSE polyofficial END AS poly,
-//       CASE
-//         WHEN polyindex =
-//              Coalesce(simplified, polygon) THEN 1
-//         ELSE St_area(St_intersection(polyindex,
-//                                     Coalesce(simplified, polygon)))
-//              / St_area(polyindex)
-//       end                          AS overlap,
-//       CASE
-//         WHEN polyindex =
-//              Coalesce(simplified, polygon) THEN 1
-//         ELSE St_area(polyindex) / St_area(
-//                     St_intersection(polyindex,
-//                             Coalesce(simplified, polygon)))
-//       end                          AS overlap2
-//FROM   groups
-//       INNER JOIN authorities
-//               ON ( polyindex =
-//                    Coalesce(simplified, polygon)
-//                     OR St_intersects(polyindex,
-//                            Coalesce(simplified, polygon)) )
-//WHERE  type = ?
-//       AND publish = 1
-//       AND onmap = 1
-//       AND authorities.id = ?
-//       AND groups.id = ?;";
-//        $dbhr->preQuery($sql, [
-//            Group::GROUP_FREEGLE,
-//            $id,
-//            $group['id']
-//        ]);
-//    } catch (\Exception $e) {
-//        error_log("Exception on #{$group['id']} {$group['nameshort']}");
-//    }
-//}
-//
 $atts = $a->getPublic();
 
 foreach ($atts['groups'] as $group) {
