3a3efa053e6d1981951710c9daf5032602058ba2
Edward Hibbert <edward@ehibbert.org.uk>
2020-12-28 16:53:11 +0000

    Case sensitivity in relevant emails

--- mailtemplates/relevant/one.php
+++ mailtemplates/relevant/one.php
@@ -3,11 +3,11 @@ namespace Freegle\Iznik;
 
 function relevant_one($subject, $href, $matched, $reason) {
     $html = "<a href=\"$href\">";
-    $s = str_replace($matched, "<b style=\"color: #FF8C00\">$matched</b>", $subject);
+    $s = str_ireplace($matched, "<b style=\"color: #FF8C00\">$matched</b>", $subject);
     $html .= "$s</a> <span style=\"color: grey\">because you ";
     $s = $reason['type'] == Relevant::MATCH_VIEWED ? $reason['term'] : preg_replace('/\[.*?\]\s*/', '', $reason['subject']);
 
-    $s = str_replace($matched, "<b style=\"color: #FF8C00\">$matched</b>", $s);
+    $s = str_ireplace($matched, "<b style=\"color: #FF8C00\">$matched</b>", $s);
     $html .= $reason['type'] == Relevant::MATCH_VIEWED ? "searched for $s " : "posted $s";
     $html .= " on " . date('d M', strtotime($reason['date']));
     $html .= "</span><br />";
