2cb32d3a51f8e65a5caa37f5d784cf70e5ebb85e
Edward Hibbert <edward@ehibbert.org.uk>
2020-12-28 16:59:19 +0000

    Reason misleading in relevant emails

--- mailtemplates/relevant/one.php
+++ mailtemplates/relevant/one.php
@@ -8,7 +8,7 @@ function relevant_one($subject, $href, $matched, $reason) {
     $s = $reason['type'] == Relevant::MATCH_VIEWED ? $reason['term'] : preg_replace('/\[.*?\]\s*/', '', $reason['subject']);
 
     $s = str_ireplace($matched, "<b style=\"color: #FF8C00\">$matched</b>", $s);
-    $html .= $reason['type'] == Relevant::MATCH_VIEWED ? "searched for $s " : "posted $s";
+    $html .= $reason['type'] == Relevant::MATCH_VIEWED ? "looked at a post containing '$s' " : "posted $s";
     $html .= " on " . date('d M', strtotime($reason['date']));
     $html .= "</span><br />";
 
