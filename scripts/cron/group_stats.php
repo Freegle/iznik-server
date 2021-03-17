d9e34521ae0b799429b0b3050762649e491af9d8
Edward Hibbert <edward@ehibbert.org.uk>
2021-03-17 11:51:48 +0000

    Remove old code

--- scripts/cron/group_stats.php
+++ scripts/cron/group_stats.php
@@ -177,22 +177,3 @@ foreach ($stats as $stat) {
         $stat['date']
     ]);
 }
-
-# Look for any dashboards which need refreshing.  Any which exist need updating now as we are run by cron
-# at the start of a new day.  Any which don't exist will be created on demand.
-#
-# No longer needed as we have switched to components.
-//$todos = $dbhr->preQuery("SELECT id, `key`, type, userid, systemwide, groupid, start FROM users_dashboard;");
-//
-//foreach ($todos as $todo) {
-//    $me = $todo['userid'] ? (new User($dbhr, $dbhm, $todo['userid'])) : NULL;
-//    $_SESSION['id'] = $me ? $me->getId() : NULL;
-//    $d = new Dashboard($dbhr, $dbhm, $me);
-//    error_log("Update dashboard user {$todo['userid']}, group {$todo['groupid']}, type {$todo['type']}, start {$todo['start']}");
-//    $d->get($todo['systemwide'], $todo['groupid'] == NULL, $todo['groupid'], NULL, $todo['type'], $todo['start'], 'today', TRUE, $todo['key']);
-//}
-
-if (RETURN_PATH) {
-    # Ensure that our seedlist is current and will be mailed.
-    $dbhm->preExec("UPDATE users SET lastaccess = NOW() WHERE id IN (SELECT userid FROM returnpath_seedlist) AND DATE(lastaccess) < CURDATE();");
-}
