9ea6a54bde5b9553970a2974b5ae3b62afc568cf
Edward Hibbert <edward@ehibbert.org.uk>
2021-03-25 11:33:42 +0000

    Fix up bad last location values

--- scripts/fix/fix_lastlocation.php
+++ scripts/fix/fix_lastlocation.php
@@ -1,10 +1,9 @@
-
 <?php
 
+namespace Freegle\Iznik;
 require_once dirname(__FILE__) . '/../../include/config.php';
 require_once(IZNIK_BASE . '/include/db.php');
-
-require_once(IZNIK_BASE . '/include/user/User.php');
+global $dbhr, $dbhm;
 
 $users = $dbhr->preQuery("SELECT id, lastlocation, settings FROM users WHERE settings IS NOT NULL;");
 
@@ -13,34 +12,41 @@ error_log(count($users) . " users");
 $total = count($users);
 $count = 0;
 
-foreach ($users as $user) {
+function fixUser($user, $dbhr, $dbhm) {
     $s = json_decode($user['settings'], TRUE);
 
     if (Utils::pres('mylocation', $s) && $s['mylocation']['id'] != $user['lastlocation']) {
         #error_log("{$user['id']} => {$s['mylocation']['id']}");
 
-        $dbhm->preExec("UPDATE users SET lastlocation = ? WHERE id = ?;", [
-            $s['mylocation']['id'],
-            $user['id']
-        ], FALSE);
+        try {
+            $dbhm->preExec("UPDATE users SET lastlocation = ? WHERE id = ?;", [
+                $s['mylocation']['id'],
+                $user['id']
+            ], FALSE);
+        } catch (\Exception $e) {
+            // Likely to be because the location is invalid.
+            error_log("User {$user['id']} failed on location #{$s['mylocation']['id']} name {$s['mylocation']['name']}");
+            $l = new Location($dbhr, $dbhm);
+            $lid = $l->findByName($s['mylocation']['name']);
+
+            if ($lid) {
+                error_log("...found as $lid");
+                $s['mylocation']['id'] = $lid;
+                $dbhm->preQuery("UPDATE users SET settings = ? WHERE id = ?;", [
+                    json_encode($s),
+                    $user['id']
+                ]);
+            } else {
+                error_log("...some other error " . $e->getMessage());
+            }
+        }
     }
 
-    $count ++;
-
-    if ($count % 1000 == 0) {
-        error_log("...$count / $total");
-    }
+    gc_collect_cycles();
 }
 
-$msgs = $dbhr->preQuery("SELECT fromuser, locationid FROM messages INNER JOIN messages_groups ON messages_groups.msgid = messages.id INNER JOIN groups ON groups.id = messages_groups.groupid AND groups.type = 'Freegle';");
-$total = count($msgs);
-$count = 0;
-
-foreach ($msgs as $msg) {
-    $dbhm->preExec("UPDATE users SET lastlocation = ? WHERE id = ? AND lastlocation IS NULL;", [
-        $msg['locationid'],
-        $msg['fromuser']
-    ]);
+foreach ($users as $user) {
+    fixUser($user, $dbhm, $dbhm);
 
     $count ++;
 
