86ef39c1f67830dc61e263c62d0073a21f95c0f7
Edward Hibbert <edward@ehibbert.org.uk>
2021-04-05 14:29:27 +0100

    UT fixes

--- test/ut/php/include/UserTest.php
+++ test/ut/php/include/UserTest.php
@@ -1365,54 +1365,6 @@ class userTest extends IznikTestCase {
         ], $u->getActiveCounts());
     }
 
-    public function testJobs() {
-        foreach (glob("/tmp/adview.*") AS $filename) {
-            unlink($filename);
-        }
-
-        $u = User::get($this->dbhr, $this->dbhm);
-        $id = $u->create('Test', 'User', NULL);
-
-        $g = new Group($this->dbhr, $this->dbhm);
-        $gid = $g->findByShortName('FreeglePlayground');
-        $g = new Group($this->dbhr, $this->dbhm, $gid);
-        $g->setPrivate('lat', 55.9533);
-        $g->setPrivate('lng', -3.1883);
-
-        # First when we have a postcode.
-        $settings = [
-            'mylocation' => [
-                'name' => 'EH3 6SS',
-                'type' => 'Postcode',
-                'lat' => 55.957571,
-                'lng' => -3.205333
-            ],
-        ];
-
-        $u->setPrivate('settings', json_encode($settings));
-
-        # Setting the postcode should generate a log.
-        $this->waitBackground();
-        $logs = [ $u->getId() => [ 'id' => $u->getId() ] ];
-        $u->getPublicLogs($u, $logs, FALSE, $ctx, FALSE, FALSE);
-        $log = $this->findLog(Log::TYPE_USER, Log::SUBTYPE_POSTCODECHANGE, $logs[$u->getId()]['logs']);
-        assertEquals('EH3 6SS', $log['text']);
-
-        $jobs = $u->getJobAds();
-        assertEquals('Edinburgh', $jobs['location']);
-
-        # And again for cache.
-        $jobs = $u->getJobAds();
-        assertEquals('Edinburgh', $jobs['location']);
-
-        # Now when we have just a group membership
-        $l = new Location($this->dbhr, $this->dbhm);
-        $u->addMembership($gid);
-        $u->setPrivate('settings', json_encode([]));
-        $jobs = $u->getJobAds();
-        assertEquals('Edinburgh', $jobs['location']);
-    }
-
     public function testHide() {
         $u = new User($this->dbhm, $this->dbhm);
         $uid = $u->create("Test", "User", "A freegler");
