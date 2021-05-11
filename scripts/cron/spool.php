b6ebf37f722d3d1b6a7a2d29d753e7f86e378c0f
Edward Hibbert <edward@ehibbert.org.uk>
2021-05-11 14:51:11 +0100

    Mail getting stuck in spooler

--- scripts/cron/spool.php
+++ scripts/cron/spool.php
@@ -12,9 +12,9 @@ $spoolname = Utils::presdef('n', $opts, '/spool');
 
 $lockh = Utils::lockScript(basename(__FILE__ . '_' . $spoolname));
 
-# Some messages can fail to send, if exim is playing up.
 $spool = new \Swift_FileSpool(IZNIK_BASE . $spoolname);
 $spool->recover(60);
+$recover = 300;
 
 do {
     try {
@@ -44,5 +44,13 @@ do {
         exit(0);
     } else {
         sleep(1);
+
+        $recover--;
+
+        if ($recover <= 0) {
+            # Some messages can fail to send, if exim is playing up.
+            $spool->recover(60);
+            $recover = 300;
+        }
     }
 } while (true);
