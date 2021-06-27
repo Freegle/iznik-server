d671b4b2ae32bb63f066bf72328439365cff92d5
Edward Hibbert <edward@ehibbert.org.uk>
2021-06-27 16:44:01 +0100

    UT fixes

--- test/ut/php/api/messageTest.php
+++ test/ut/php/api/messageTest.php
@@ -2788,8 +2788,8 @@ class messageAPITest extends IznikAPITestCase
 
         assertEquals(0, $ret['ret']);
         assertEquals('Test edit', $ret['message']['textbody']);
-        assertEquals(56.09999995793, $ret['message']['lat']);
-        assertEquals(1.2332270226975, $ret['message']['lng']);
+        assertEquals(56.09999983172034, $ret['message']['lat']);
+        assertEquals(1.2364540449459, $ret['message']['lng']);
     }
 
     public function testPartnerConsent() {
@@ -2866,8 +2866,8 @@ class messageAPITest extends IznikAPITestCase
         ]);
 
         # Lat/lng  blurred.
-        assertEquals(8.531557487729005, $ret['message']['lat']);
-        assertEquals(179.21638396072, $ret['message']['lng']);
+        assertEquals(8.529784975202238, $ret['message']['lat']);
+        assertEquals(179.2160679249, $ret['message']['lng']);
         assertFalse(array_key_exists('location', $ret['message']));
         assertEquals(1, count($ret['message']['fromuser']['emails']));
         assertEquals($email, $ret['message']['fromuser']['emails'][0]['email']);
