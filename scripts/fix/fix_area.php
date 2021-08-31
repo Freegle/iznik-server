4401f76d660cd51e0dbbdbe314e151a91dfab4cb
Edward Hibbert <edward@ehibbert.org.uk>
2021-08-31 16:39:36 +0100

    Fix some mapping areas

--- scripts/fix/fix_area.php
+++ scripts/fix/fix_area.php
@@ -1,16 +1,21 @@
 <?php
+namespace Freegle\Iznik;
 
 require_once dirname(__FILE__) . '/../../include/config.php';
 require_once(IZNIK_BASE . '/include/db.php');
 
 require_once(IZNIK_BASE . '/include/misc/Location.php');
+global $dbhr, $dbhm;
 
-$locs = $dbhr->query("SELECT id FROM locations WHERE areaid = (SELECT id FROM locations WHERE name LIKE 'TV13');");
+$locs = $dbhr->query("SELECT id FROM locations WHERE areaid = 9683163;");
 
 $count = 0;
+
 foreach ($locs as $loc) {
     $l = new Location($dbhr, $dbhm);
     $l->setParents($loc['id']);
+    $count++;
+    error_log("$count ");
 }
 
 
