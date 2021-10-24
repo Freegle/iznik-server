967c48eb7366fa3c7fa66fe89d6eee827255b3e6
Edward Hibbert <edward@ehibbert.org.uk>
2021-10-24 22:05:58 +0100

    Add locking round preview cron script

--- scripts/cron/previews.php
+++ scripts/cron/previews.php
@@ -8,5 +8,9 @@ require_once(BASE_DIR . '/include/config.php');
 require_once(IZNIK_BASE . '/include/db.php');
 global $dbhr, $dbhm;
 
+$lockh = Utils::lockScript(basename(__FILE__));
+
 $n = new Newsfeed($dbhr, $dbhm);
 $n->updatePreviews();
+
+Utils::unlockScript($lockh);
