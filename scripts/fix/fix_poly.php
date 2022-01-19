0f2aff5c88164c53f0f7e68bc2c035dd17a3095c
Edward Hibbert <edward@ehibbert.org.uk>
2022-01-19 09:00:51 +0000

    Fix some group polygons.

--- scripts/fix/fix_poly.php
+++ scripts/fix/fix_poly.php
@@ -2,21 +2,20 @@
 
 require_once dirname(__FILE__) . '/../../include/config.php';
 require_once(IZNIK_BASE . '/include/db.php');
+global $dbhr, $dbhm;
 
-require_once(IZNIK_BASE . '/include/group/Group.php');
-
-$groups = $dbhr->preQuery("SELECT id FROM `groups` ORDER BY id;");
+$groups = $dbhr->preQuery("SELECT id, nameshort FROM `groups` ORDER BY id;");
 
 foreach ($groups as $group) {
-    error_log($group['id']);
-    $g = $dbhr->preQuery("SELECT  ST_AsText(ST_GeomFromText(COALESCE(poly, polyofficial, 'POINT(0 0)'))) AS geomtext FROM `groups` WHERE id = ?;", [
+    $g = $dbhr->preQuery("SELECT ST_AsText(polyindex) AS current, ST_AsText(ST_GeomFromText(COALESCE(poly, polyofficial, 'POINT(0 0)'))) AS geomtext FROM `groups` WHERE id = ?;", [
         $group['id']
     ]);
 
-    $dbhm->preExec("UPDATE `groups` SET polyindex = ST_GeomFromText(?, {$dbhr->SRID()}) WHERE id = ?;", [
-        $g[0]['geomtext'],
-        $group['id']
-    ]);
-
-    #error_log("...{$g[0]['geomtext']}");
+    if ($g[0]['current'] != $g[0]['geomtext']) {
+        error_log("Wrong for {$group['nameshort']}");
+        $dbhm->preExec("UPDATE `groups` SET polyindex = ST_GeomFromText(?, {$dbhr->SRID()}) WHERE id = ?;", [
+            $g[0]['geomtext'],
+            $group['id']
+        ]);
+    }
 }
