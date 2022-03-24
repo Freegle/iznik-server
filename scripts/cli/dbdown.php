a8c528da09e3b981bbbac70f396f984f9c3ac34c
edwh <edward@ehibbert.org.uk>
2022-03-24 13:03:27 +0000

    Script to stop using a DB server

---
+++ scripts/cli/dbdown.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Freegle\Iznik;
+
+define('BASE_DIR', dirname(__FILE__) . '/../..');
+require_once(BASE_DIR . '/include/config.php');
+
+require_once(IZNIK_BASE . '/include/db.php');
+global $dbhr, $dbhm;
+
+$opts = getopt('h:');
+
+if (count($opts) < 1) {
+    echo "Usage: php dbdown.php -h <1-3>\n";
+} else {
+    $host = "db{$opts['h']}";
+
+    while (true) {
+        touch("/tmp/iznik.dbstatus.$host:3306.down");
+        sleep(10);
+    }
+}
