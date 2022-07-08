ad55dfea4d321cdc9c83b4b098317e2d371b1109
Edward Hibbert <edward@ehibbert.org.uk>
2022-07-08 09:59:17 +0100

    Script to mail story info.

---
+++ scripts/cli/stories_mail.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Freegle\Iznik;
+
+define('BASE_DIR', dirname(__FILE__) . '/../..');
+require_once(BASE_DIR . '/include/config.php');
+
+require_once(IZNIK_BASE . '/include/db.php');
+global $dbhr, $dbhm;
+
+$opts = getopt('i:e:');
+
+if (count($opts)) {
+    $ids = explode(',', $opts['i']);
+    $email = $opts['e'];
+    $text = '';
+
+    foreach ($ids as $id) {
+        $s = new Story($dbhr, $dbhm, $id);
+
+        if ($s->getId() == $id) {
+            $u = new User($dbhr, $dbhm, $s->getPrivate('userid'));
+            $text .= "Story #$id\r\n\r\n";
+            $text .= "Name: " . $u->getName() . "\r\n";
+            $text .= "Email: " . $u->getEmailPreferred() . "\r\n";
+            $text .= "Last active: " . $u->getPrivate('lastaccess') . "\r\n";
+            $text .= "Location: " . $u->getPublicLocation()['display'] . "\r\n";
+            $members = $u->getMemberships();
+
+            foreach ($members as $member) {
+                $text .= "Member of: " . $member['namedisplay'] . "\r\n";
+            }
+
+            $text .= "\r\n\r\n" . $s->getPrivate('headline') . "\r\n\r\n" . $s->getPrivate('story') . "\r\n\r\n===========\r\n\r\n";
+        } else {
+            error_log("Invalid id $id");
+        }
+    }
+
+    $message = \Swift_Message::newInstance()
+        ->setSubject("Story information")
+        ->setFrom(NOREPLY_ADDR)
+        ->setTo($email)
+        ->setBody($text);
+
+    list ($transport, $mailer) = Mail::getMailer();
+
+    $mailer->send($message);
+}
