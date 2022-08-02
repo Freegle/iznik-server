7c40c5b173e8f1d5c459894b074c190d4acffd59
Edward Hibbert <edward@ehibbert.org.uk>
2022-08-02 09:40:06 +0100

    Test fixes.

--- include/group/Alert.php
+++ include/group/Alert.php
@@ -331,7 +331,8 @@ class Alert extends Entity
                     $this->alert['html'],
                     NULL,
                     $this->alert['askclick'] ? 'https://' . USER_SITE . "/alert/viewed/$trackid" : NULL,
-                    'https://' . USER_SITE . "/beacon/$trackid");
+                    'https://' . USER_SITE . "/beacon/$trackid",
+                    !$groupid);
 
                 $text = $this->alert['text'];
                 if ($this->alert['askclick']) {
@@ -359,7 +360,8 @@ class Alert extends Entity
                 $this->alert['html'] ? $this->alert['html'] : nl2br($this->alert['text']),
                 NULL,
                 FALSE,
-                'https://' . USER_SITE);
+                'https://' . USER_SITE,
+                !$groupid);
 
             $text = $this->alert['text'];
             $msg = $this->constructMessage($from, $g->getPrivate('nameshort'), NULL, $from, $this->alert['subject'], $text, $html);
