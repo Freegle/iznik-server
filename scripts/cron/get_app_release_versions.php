428deaeecb1587d03b5510ac73aa81253e04966b
Chris Cant <chris@phdcc.com>
2022-08-03 09:51:10 +0100

    Fix Android get app versions to scrape updated listing format

--- scripts/cron/get_app_release_versions.php
+++ scripts/cron/get_app_release_versions.php
@@ -60,10 +60,13 @@ try{
   //        const FD_LOOKUP = 'https://play.google.com/store/apps/details?id=org.ilovefreegle.direct&hl=en'
   //        const MT_LOOKUP = 'https://play.google.com/store/apps/details?id=org.ilovefreegle.modtools&hl=en'
 
-  $lookfor = '[null,null,[]],null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,[[["';
+  //$lookfor = '[null,null,[]],null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,[[["';
+  //$lookforlen = strlen($lookfor);
+
+  $lookfor = 'You can request that data be deleted"]]],null,null,null,[[["';
   $lookforlen = strlen($lookfor);
 
-  $lookfor2 = ']]]],[["';
+  $lookfor2 = '"]],[[[30,"11"]],[[[22,"5.1"]]]],[["';
   $lookfor2len = strlen($lookfor2);
 
   // '[[["2.0.102"]],[[[30,"11"]],[[[22,"5.1"]]]],[["May 15, 2022"]]]'
