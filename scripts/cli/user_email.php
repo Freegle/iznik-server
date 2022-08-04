36640dace2a7b15faa6f15065657fbdec78a010c
Edward Hibbert <edward@ehibbert.org.uk>
2022-08-04 07:43:03 +0100

    Allow add of non-primary email

--- scripts/cli/user_email.php
+++ scripts/cli/user_email.php
@@ -8,15 +8,16 @@ require_once(BASE_DIR . '/include/config.php');
 require_once(IZNIK_BASE . '/include/db.php');
 global $dbhr, $dbhm;
 
-$opts = getopt('e:a:r:i:');
+$opts = getopt('e:a:r:i:p:');
 
 if (count($opts) < 1) {
-    echo "Usage: php user_email.php (-e <email to find> or -i <user id>) (-a <email to add> -r <email to remove>\n";
+    echo "Usage: php user_email.php (-e <email to find> or -i <user id>) (-a <email to add> -r <email to remove> -p <primary>\n";
 } else {
     $uid = Utils::presdef('i', $opts, NULL);
     $find = Utils::presdef('e', $opts, NULL);
     $add = Utils::presdef('a', $opts, NULL);
     $remove = Utils::presdef('r', $opts, NULL);
+    $primary = Utils::presdef('p', $opts, 0);
     $u = User::get($dbhr, $dbhm);
     $uid = $uid ? $uid : $u->findByEmail($find);
 
@@ -25,7 +26,7 @@ if (count($opts) < 1) {
         $u = User::get($dbhr, $dbhm, $uid);
 
         if ($add) {
-            $rc = $u->addEmail($add);
+            $rc = $u->addEmail($add, $primary, $primary);
             error_log("Added email $add id $rc");
         }
 
