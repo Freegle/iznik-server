6952c8b8e2103da0dcf8734f5a1d427407f02bc2
Edward Hibbert <edward@ehibbert.org.uk>
2022-11-09 08:34:32 +0000

    Improve script to manually set up admins.

--- scripts/cli/admin.php
+++ scripts/cli/admin.php
@@ -8,22 +8,25 @@ require_once(BASE_DIR . '/include/config.php');
 require_once(IZNIK_BASE . '/include/db.php');
 global $dbhr, $dbhm;
 
-$opts = getopt('g:s:b:');
+$opts = getopt('g:s:b:l:t:');
 
 if (count($opts) < 2) {
-    echo "Usage: php admin.php -g <CSL id of group> -s <subject> -b <body>\n";
+    echo "Usage: php admin.php -g <CSL id of group> -s <subject> -b <filename containing body> -l <CTA link> -t <CTA text>\n";
 } else {
     $gids = $opts['g'];
+    error_log("Groups $gids");
 
     foreach (explode(',', $gids) as $gid) {
         $g = Group::get($dbhr, $dbhm, $gid);
         $subj = $opts['s'];
-        $body = $opts['b'];
+        $body = file_get_contents($opts['b']);
+        $ctalink = $opts['l'];
+        $ctatext = $opts['t'];
 
         if ($g->getId() == $gid && $subj && $body) {
-            error_log($g->getName());
+            error_log("...process " . $g->getName());
             $a = new Admin($dbhr, $dbhm);
-            $a->create($gid, NULL, $subj, $body);
+            $a->create($gid, NULL, $subj, $body, $ctatext, $ctalink);
         } else {
             error_log("Couldn't find group $gid.");
         }
