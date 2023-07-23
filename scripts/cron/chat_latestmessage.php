2b9483c835897d49c2d8bad1c7b12b5f9dacf3ef
Edward Hibbert <edward@ehibbert.org.uk>
2023-07-23 12:03:34 +0100

    Ensure chats between users and mods which have unread messages are open.

--- scripts/cron/chat_latestmessage.php
+++ scripts/cron/chat_latestmessage.php
@@ -9,7 +9,9 @@ require_once(IZNIK_BASE . '/include/db.php');
 global $dbhr, $dbhm;
 
 # This is a fallback, so it's only run occasionally through cron.
-
+#
+# Ensure the counts are correct in the chat_room.  This is no longer relevant to FD because of the Go API, but
+# is relevant to MT.
 $mysqltime = date("Y-m-d", strtotime("31 days ago"));
 $chatids = $dbhr->preQuery("SELECT DISTINCT chatid FROM chat_messages WHERE date >= ?;", [ $mysqltime ]);
 
@@ -25,4 +27,26 @@ foreach ($chatids as $chatid) {
     if ($count % 1000 === 0) {
         error_log("...$count / $total");
     }
+}
+
+# If there are any User2Mod chats which have been closed by the user but which have unseen messages, then we
+# need to reopen them.  This shouldn't arise very often as we hide the close button if there are unseen messages,
+# and reopen them when we create a message.  But we want to make very sure that messages from mods are seen.
+$chats = $dbhr->preQuery("SELECT DISTINCT chat_rooms.id, chat_roster.status, chat_roster.lastmsgseen, chat_rooms.user1 
+    FROM `chat_rooms` 
+    INNER JOIN chat_roster ON chat_roster.chatid = chat_rooms.id 
+    WHERE chat_rooms.user1 = chat_roster.userid 
+        AND chat_roster.status = ?
+        AND chattype = ?
+        AND chat_rooms.latestmessage >= chat_roster.date 
+        AND chat_rooms.latestmessage >= ?;", [ ChatRoom::STATUS_CLOSED, ChatRoom::TYPE_USER2MOD, $mysqltime ]);
+
+error_log("Found chats which need reopening " . count($chats));
+
+foreach ($chats as $chat) {
+    $dbhm->preExec("UPDATE chat_roster SET status = ? WHERE chatid = ? AND userid = ?;", [
+        ChatRoom::STATUS_AWAY,
+        $chat['id'],
+        $chat['user1']
+    ]);
 }
