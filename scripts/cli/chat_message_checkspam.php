af0b6b8fb45cb9cb41377a9025ce4012047a6a33
edwh <edward@ehibbert.org.uk>
2023-11-07 08:40:07 +0000

    Script to check chat spam.

---
+++ scripts/cli/chat_message_checkspam.php
@@ -0,0 +1,30 @@
+<?php
+# Rescale large images in message_attachments
+
+namespace Freegle\Iznik;
+
+define('BASE_DIR', dirname(__FILE__) . '/../..');
+require_once(BASE_DIR . '/include/config.php');
+require_once(IZNIK_BASE . '/include/db.php');
+
+global $dbhr, $dbhm;
+
+$opts = getopt('i:');
+
+if (count($opts) < 1) {
+    echo "Usage: php chat_message_checkspam.php -i <chat message id>\n";
+} else {
+    $id = Utils::presdef('i', $opts, null);
+}
+
+$messages = $dbhr->preQuery("SELECT * FROM chat_messages WHERE id = ?", [
+    $id
+]);
+
+$m = new ChatMessage($dbhr, $dbhm);
+
+foreach ($messages as $message) {
+    $reason = $m->checkSpam($message['message']);
+
+    error_log("Reason? $reason");
+}
