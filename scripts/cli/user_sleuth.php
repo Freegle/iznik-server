644091b28b5722f8886be0ad8d853cc6361fb591
Edward Hibbert <edward@ehibbert.org.uk>
2024-01-08 18:57:39 +0000

    Donation sleuthing script.

--- scripts/cli/user_sleuth.php
+++ scripts/cli/user_sleuth.php
@@ -1,5 +1,5 @@
 <?php
-
+# php user_sleuth.php -f "First Last" -s Last -p 50 -g 30 -d 2023-11-13
 namespace Freegle\Iznik;
 
 define('BASE_DIR', dirname(__FILE__) . '/../..');
@@ -8,58 +8,74 @@ require_once(BASE_DIR . '/include/config.php');
 require_once(IZNIK_BASE . '/include/db.php');
 global $dbhr, $dbhm;
 
-function addPoss(&$poss, $users)
+$opts = getopt('f:s:d:p:g:');
+
+$fn = trim($opts['f']);
+$sn = trim($opts['s']);
+$date = trim($opts['d']);
+$maxPoss = intval($opts['p']) ?: 5;
+$grace = intval($opts['g']) ?: 7;
+
+function addPoss($poss, $users, $maxPoss = 5)
 {
     global $dbhr, $dbhm;
 
-    if (count($users) && count($users) < 5) {
+    if (count($users) && count($users) < $maxPoss) {
         foreach ($users as $user) {
             $u = User::get($dbhr, $dbhm, $user['id']);
 
             $poss[$user['id']] = $user['displayname'] . " (" . $u->getEmailPreferred() . ")";
         }
     }
+
+    return $poss;
 }
 
-function showPoss($fn, $sn, $poss) {
+function showPoss($fn, $sn, $poss, $maxPoss) {
     $poss = array_unique($poss);
 
-    if (count($poss) && count($poss) <= 5) {
+    if (count($poss) && count($poss) <= $maxPoss) {
         error_log("$fn $sn possibilities:");
 
-        foreach ($poss as $userid => $name) {
+        foreach ($poss as $userid => $name)
+        {
             error_log("  #$userid $name");
         }
+    } else if (count($poss) > $maxPoss) {
+        error_log("Too many possibilities (" . count($poss) . ")");
+    } else {
+        error_log("No possibilities found");
     }
 }
 
-if (($handle = fopen("/tmp/sleuth.csv", "r")) !== FALSE)
-{
-    fgetcsv($handle);
+if (strlen($fn) && strlen($sn)) {
+    $poss = [];
 
-    while (($data = fgetcsv($handle, 1000, ",")) !== false) {
-        $fn = trim($data[4]);
-        $sn = trim($data[5]);
+    $users = $dbhr->preQuery("SELECT id, CASE WHEN users.fullname IS NOT NULL THEN users.fullname ELSE CONCAT(users.firstname, ' ', users.lastname) END AS displayname FROM users WHERE lastname LIKE ? AND ABS(DATEDIFF(lastaccess, ?)) < ?", [
+        $sn,
+        $date,
+        $grace
+    ]);
 
-        if (strlen($fn) && strlen($sn)) {
-            $poss = [];
+    $poss = addPoss($poss, $users, $maxPoss);
 
-            $users = $dbhr->preQuery("SELECT id, CASE WHEN users.fullname IS NOT NULL THEN users.fullname ELSE CONCAT(users.firstname, ' ', users.lastname) END AS displayname FROM users WHERE lastname LIKE ?", [
-                $sn
-            ]);
+    $users = $dbhr->preQuery("SELECT id, CASE WHEN users.fullname IS NOT NULL THEN users.fullname ELSE CONCAT(users.firstname, ' ', users.lastname) END AS displayname  FROM users WHERE fullname LIKE '%$sn' AND ABS(DATEDIFF(lastaccess, ?)) < ?", [
+        $date,
+        $grace
+    ]);
+    $poss = addPoss($poss, $users, $maxPoss);
 
-            addPoss($poss, $users);
+    $users = $dbhr->preQuery("SELECT id, CASE WHEN users.fullname IS NOT NULL THEN users.fullname ELSE CONCAT(users.firstname, ' ', users.lastname) END AS displayname  FROM users WHERE fullname LIKE '$fn $sn' AND ABS(DATEDIFF(lastaccess, ?)) < ?", [
+        $date,
+        $grace
+    ]);
+    $poss = addPoss($poss, $users, $maxPoss);
 
-            $users = $dbhr->preQuery("SELECT id, CASE WHEN users.fullname IS NOT NULL THEN users.fullname ELSE CONCAT(users.firstname, ' ', users.lastname) END AS displayname  FROM users WHERE fullname LIKE '%$sn'");
-            addPoss($poss, $users);
+    $users = $dbhr->preQuery("SELECT users.id, CASE WHEN users.fullname IS NOT NULL THEN users.fullname ELSE CONCAT(users.firstname, ' ', users.lastname) END AS displayname  FROM users INNER JOIN users_emails ON users_emails.userid = users.id WHERE email LIKE '$fn$sn%' AND ABS(DATEDIFF(lastaccess, ?)) < ?", [
+        $date,
+        $grace
+    ]);
+    $poss = addPoss($poss, $users, $maxPoss);
 
-            $users = $dbhr->preQuery("SELECT id, CASE WHEN users.fullname IS NOT NULL THEN users.fullname ELSE CONCAT(users.firstname, ' ', users.lastname) END AS displayname  FROM users WHERE fullname LIKE '$fn $sn'");
-            addPoss($poss, $users);
-
-            $users = $dbhr->preQuery("SELECT users.id, CASE WHEN users.fullname IS NOT NULL THEN users.fullname ELSE CONCAT(users.firstname, ' ', users.lastname) END AS displayname  FROM users INNER JOIN users_emails ON users_emails.userid = users.id WHERE email LIKE '$fn$sn%'");
-            addPoss($poss, $users);
-
-            showPoss($fn, $sn, $poss);
-        }
-    }
+    showPoss($fn, $sn, $poss, $maxPoss);
 }
