25c77a59ac4d5f86b1b56c8555aa23d81394eca2
edwh <edward@ehibbert.org.uk>
2024-01-15 08:05:52 +0000

    Script to reset obsolete holiday field.

---
+++ scripts/fix/fix_holiday.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Freegle\Iznik;
+
+define('BASE_DIR', dirname(__FILE__) . '/../..');
+require_once(BASE_DIR . '/include/config.php');
+
+require_once(IZNIK_BASE . '/include/db.php');
+global $dbhr, $dbhm;
+
+$users = $dbhr->preQuery("SELECT id FROM users WHERE onholidaytill is not null and onholidaytill > now();");
+
+foreach ($users as $user) {
+    $dbhm->preExec("UPDATE users SET onholidaytill = NULL WHERE id = ?", [
+        $user['id']
+    ]);
+}
