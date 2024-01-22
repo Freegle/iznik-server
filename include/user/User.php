975a07ccca1d0950069950e04bb73883848519c4
Edward Hibbert <edward@ehibbert.org.uk>
2024-01-22 10:14:12 +0000

    Allow link logins for deleted users.

--- include/user/User.php
+++ include/user/User.php
@@ -261,7 +261,7 @@ class User extends Entity
             # We're not already logged in as this user.
             $ret = FALSE;
 
-            $sql = "SELECT users_logins.* FROM users_logins INNER JOIN users ON users.id = users_logins.userid WHERE userid = ? AND type = ? AND credentials = ? AND users.deleted IS NULL;";
+            $sql = "SELECT users_logins.* FROM users_logins INNER JOIN users ON users.id = users_logins.userid WHERE userid = ? AND type = ? AND credentials = ?;";
 
             $logins = $this->dbhr->preQuery($sql, [$this->id, User::LOGIN_LINK, $key]);
             foreach ($logins as $login) {
