841862e9cf6eefce246fe71d2be3c7731eecf87c
Edward Hibbert <edward@ehibbert.org.uk>
2024-05-02 12:32:37 +0100

    Only show popular posts which were added recently.

--- include/group/Group.php
+++ include/group/Group.php
@@ -1319,7 +1319,7 @@ HAVING logincount > 0
             $msgs = $this->dbhr->preQuery("SELECT messages_popular.* FROM messages_popular
             INNER JOIN groups_facebook ON groups_facebook.groupid = messages_popular.groupid
             INNER JOIN messages ON messages.id = messages_popular.msgid 
-            WHERE TIMESTAMPDIFF(HOUR, messages.arrival, NOW()) <= 48 AND messages_popular.groupid IN (" . implode(',', $gids) . ") AND shared = 0 AND declined = 0 AND expired = 0 AND deleted IS NULL
+            WHERE TIMESTAMPDIFF(HOUR, NOW(), messages.arrival) <= 48 AND messages_popular.groupid IN (" . implode(',', $gids) . ") AND shared = 0 AND declined = 0 AND expired = 0 AND deleted IS NULL
             GROUP BY messages_popular.id;");
 
             $ret = [];
