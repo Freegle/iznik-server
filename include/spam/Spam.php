a420d804cd0bde6505bc353ea8b29e6e79aacf72
Edward Hibbert <edward@ehibbert.org.uk>
2024-05-19 12:26:53 +0100

    More anti-spam

--- include/spam/Spam.php
+++ include/spam/Spam.php
@@ -828,7 +828,9 @@ class Spam {
         }
 
         # Find any chat messages from spammers.
-        $chats = $this->dbhr->preQuery("SELECT id, chatid FROM chat_messages WHERE userid IN (SELECT userid FROM spam_users WHERE collection = 'Spammer');");
+        $chats = $this->dbhr->preQuery("SELECT id, chatid FROM chat_messages WHERE 
+userid IN (SELECT userid FROM spam_users WHERE collection = 'Spammer')
+AND reviewrequested != 1;");
         foreach ($chats as $chat) {
             error_log("Found spam chat message {$chat['id']}");
             $sql = "UPDATE chat_messages SET reviewrejected = 1, reviewrequired = 0 WHERE id = ?";
