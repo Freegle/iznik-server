c1c5cdd1eabb17316ea7b1f4abad091997e079ca
Edward Hibbert <edward@ehibbert.org.uk>
2024-07-19 14:47:09 +0100

    Remove dead code.

--- include/misc/UploadCare.php
+++ include/misc/UploadCare.php
@@ -14,45 +14,6 @@ class UploadCare {
         }
     }
 
-    function upload($data, $mimeType) {
-        $file = $this->uploaderApi->fromContent($data, $mimeType);
-        $uid = $file->getUuid();
-
-        return $uid;
-    }
-
-    function stripExif($uid) {
-        # We want to strip the EXIF data.  We remove all of it to avoid any privacy issues.  You have to
-        # add preview as an operation to make it work.
-        #
-        # syncUploadFromUrl guarantees that the image is available on the CDN before returning.
-//        $oldFileInfo = $this->fileApi->fileInfo($uid);
-//        $newFileInfo = $this->uploaderApi->syncUploadFromUrl(UPLOADCARE_CDN . "$uid/-/strip_meta/all/-/preview/");
-//        $newuid = $newFileInfo->getUuid();
-//        $this->fileApi->storeFile($newuid);
-//        #error_log("Copy $uid, $url to $newuid, $newurl, ready " . $newFileInfo->isReady());
-//
-//        if ($newuid) {
-//            $this->fileApi->deleteFile($oldFileInfo);
-//        }
-//
-//        return $newuid;
-
-        // Disabling to investigate the effect on costs.  Affects imageTest:229.
-        return $uid;
-    }
-
-    function getPerceptualHash($uid) {
-        $json  = file_get_contents(UPLOADCARE_CDN . "$uid/-/json/");
-
-        if ($json) {
-            $data = json_decode($json, true);
-            return $data['hash'];
-        }
-
-        return NULL;
-    }
-
     function getUrl($uid, $mods) {
         # Construct the external URL from the UID and mods.
         $url = UPLOADCARE_CDN . "$uid/";
