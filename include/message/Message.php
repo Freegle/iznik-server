cc15f4480c6f440837e6bfc00ead0e7466bf07de
Edward Hibbert <edward@ehibbert.org.uk>
2024-08-06 16:51:16 +0100

    Some messages where the user has been deleted showing up for unread counts.

--- include/message/Message.php
+++ include/message/Message.php
@@ -5509,8 +5509,8 @@ $mq", [
         # Remove any messages which are deleted, or where the user has been deleted
         $sql = "SELECT DISTINCT messages_spatial.id FROM messages_spatial 
     INNER JOIN messages ON messages_spatial.msgid = messages.id 
-    INNER JOIN users ON users.id = messages.fromuser
-    WHERE messages.deleted IS NOT NULL OR users.deleted IS NOT NULL";
+    LEFT JOIN users ON users.id = messages.fromuser
+    WHERE messages.fromuser IS NULL OR messages.deleted IS NOT NULL OR users.deleted IS NOT NULL";
         $msgs = $this->dbhr->preQuery($sql);
 
         foreach ($msgs as $msg) {
