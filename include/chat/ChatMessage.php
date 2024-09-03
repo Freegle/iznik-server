c755465bccf7cc5f9cc94dc223a8a75ac17004eb
Edward Hibbert <edward@ehibbert.org.uk>
2024-09-03 12:15:33 +0100

    MT: Count for quick chat review out of sync with what you can see.

--- include/chat/ChatMessage.php
+++ include/chat/ChatMessage.php
@@ -751,8 +751,10 @@ WHERE chat_messages.chatid = ? AND chat_messages.userid != ? AND seenbyall = 0 A
     INNER JOIN chat_rooms ON reviewrequired = 1 AND reviewrejected = 0 AND chat_rooms.id = chat_messages.chatid 
     LEFT JOIN chat_messages_held ON chat_messages.id = chat_messages_held.msgid
     INNER JOIN memberships ON memberships.userid = (CASE WHEN chat_messages.userid = chat_rooms.user1 THEN chat_rooms.user2 ELSE chat_rooms.user1 END)  
-    INNER JOIN `groups` ON memberships.groupid = groups.id AND groups.type = ? WHERE chat_messages.date > '$mysqltime' $wideq AND chat_messages_held.id IS NULL ";
+    INNER JOIN `groups` ON memberships.groupid = groups.id AND groups.type = ? WHERE chat_messages.date > '$mysqltime' $wideq AND chat_messages_held.id IS NULL 
+    AND chat_messages.reportreason NOT IN (?)";
                     $params[] = Group::GROUP_FREEGLE;
+                    $params[] = ChatMessage::REVIEW_USER;
                 }
 
                 $sql .= "    ORDER BY groupid;";
