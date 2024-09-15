eb4f1c90b510861f0afd1d15f87a1ec9b21ffaba
Edward Hibbert <edward@ehibbert.org.uk>
2024-09-15 11:40:40 +0100

    User create CLI command

---
+++ scripts/cli/user_create.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Freegle\Iznik;
+
+define('BASE_DIR', dirname(__FILE__) . '/../..');
+require_once(BASE_DIR . '/include/config.php');
+
+require_once(IZNIK_BASE . '/include/db.php');
+global $dbhr, $dbhm;
+
+$opts = getopt('e:n:p:');
+
+if (count($opts) < 3) {
+    echo "Usage: php user_create.php -e <email> -n <full name> -p password\n";
+} else {
+    $email = Utils::presdef('e', $opts, NULL);
+    $name = Utils::presdef('n', $opts, NULL);
+    $password = Utils::presdef('p', $opts, NULL);
+
+    $u = User::get($dbhr, $dbhm);
+    $uid = $u->findByEmail($email);
+
+    if ($uid) {
+        error_log("User already exists for $email");
+    } else {
+        $uid = $u->create(NULL, NULL, $name);
+        $u->addEmail($email);
+        $u->addLogin(User::LOGIN_NATIVE, NULL, $password);
+        error_log("Created ". $uid);
+    }
+}
