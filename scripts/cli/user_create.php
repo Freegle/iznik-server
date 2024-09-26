1b2f089fcadfe0b7a1f98b80692d6322036afcdd
Edward Hibbert <edward@ehibbert.org.uk>
2024-09-26 14:07:56 +0100

    Allow add to group on user create.

--- scripts/cli/user_create.php
+++ scripts/cli/user_create.php
@@ -8,14 +8,15 @@ require_once(BASE_DIR . '/include/config.php');
 require_once(IZNIK_BASE . '/include/db.php');
 global $dbhr, $dbhm;
 
-$opts = getopt('e:n:p:');
+$opts = getopt('e:n:p:g:');
 
 if (count($opts) < 2) {
-    echo "Usage: php user_create.php -e <email> -n <full name> (-p <password>)\n";
+    echo "Usage: php user_create.php -e <email> -n <full name> (-p <password>) (-g <shortname group to add>)\n";
 } else {
     $email = Utils::presdef('e', $opts, NULL);
     $name = Utils::presdef('n', $opts, NULL);
     $password = Utils::presdef('p', $opts, NULL);
+    $group = Utils::presdef('g', $opts, NULL);
 
     $u = User::get($dbhr, $dbhm);
 
@@ -36,5 +37,17 @@ if (count($opts) < 2) {
         $u->addLogin(User::LOGIN_NATIVE, NULL, $password);
         $u->welcome($email, $password);
         error_log("Created ". $uid);
+
+        if ($group) {
+            $g = new Group($dbhr, $dbhm);
+            $gid = $g->findByShortName($group);
+
+            if ($gid) {
+                $u->addMembership($gid);
+                error_log("Added $email to $group");
+            } else {
+                error_log("Group $group not found");
+            }
+        }
     }
 }
