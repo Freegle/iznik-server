33a4fcd8af9a4eea5562d9e63b57bd08807878b7
Edward Hibbert <edward@ehibbert.org.uk>
2024-11-25 10:23:14 +0000

    Test fixes.

--- test/ut/php/include/chatRoomsTest.php
+++ test/ut/php/include/chatRoomsTest.php
@@ -24,6 +24,7 @@ class chatRoomsTest extends IznikTestCase {
 
         $dbhm->preExec("DELETE FROM chat_rooms WHERE name = 'test';");
         $dbhm->preExec("DELETE FROM users_emails WHERE email LIKE 'test2@user.trashnothing.com';");
+        $dbhm->preExec("DELETE FROM users_replytime;");
 
         $g = Group::get($dbhr, $dbhm);
         $this->groupid = $g->create('testgroup', Group::GROUP_FREEGLE);
