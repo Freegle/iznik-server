54803e0aefd581e055614c261de78033370bc43f
Edward Hibbert <edward@ehibbert.org.uk>
2024-12-06 09:05:52 +0000

    Alert confirmation button goes to user site rather than MT.

--- include/group/Alert.php
+++ include/group/Alert.php
@@ -287,8 +287,8 @@ class Alert extends Entity
                                 $this->alert['subject'],
                                 $this->alert['html'] ? $this->alert['html'] : nl2br($this->alert['text']),
                                 NULL, # Should be $u->getUnsubLink(USER_SITE, $mod['userid']) once we go live TODO ,
-                                $this->alert['askclick'] ? 'https://' . USER_SITE . "/alert/viewed/$trackid" : NULL,
-                                'https://' . USER_SITE . "/beacon/$trackid",
+                                $this->alert['askclick'] ? 'https://' . MOD_SITE . "/alert/viewed/$trackid" : NULL,
+                                'https://' . MOD_SITE . "/beacon/$trackid",
                                 $global);
 
                             $text = $this->alert['text'];
