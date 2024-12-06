72a2081a92d13c7bce371738e70e05f2e3f0c462
Edward Hibbert <edward@ehibbert.org.uk>
2024-12-06 12:03:42 +0000

    Don't thank for old donations.

--- scripts/cron/donations_thank.php
+++ scripts/cron/donations_thank.php
@@ -10,8 +10,13 @@ global $dbhr, $dbhm;
 
 $lockh = Utils::lockScript(basename(__FILE__));
 
-# Only thank each user once.
-$users = $dbhr->preQuery("SELECT DISTINCT users_donations.userid FROM users_donations LEFT OUTER JOIN users_thanks ON users_thanks.userid = users_donations.userid WHERE users_donations.userid IS NOT NULL AND users_thanks.userid IS NULL AND payer != 'ppgfukpay@paypalgivingfund.org';");
+# Only thank each user once.  Only thank donations within the last 7 days to avoid weirdly thanking someone for
+# an ancient donation.
+$users = $dbhr->preQuery("SELECT DISTINCT users_donations.userid FROM users_donations 
+   LEFT OUTER JOIN users_thanks ON users_thanks.userid = users_donations.userid 
+   WHERE users_donations.timestamp > DATE_SUB(NOW(), INTERVAL 7 DAY) AND 
+   users_donations.userid IS NOT NULL AND users_thanks.userid IS NULL AND payer != 'ppgfukpay@paypalgivingfund.org';");
+
 foreach ($users as $user) {
     $u = User::get($dbhr, $dbhm, $user['userid']);
     error_log($u->getEmailPreferred());
