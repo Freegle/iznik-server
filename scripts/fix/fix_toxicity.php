c97cb085eef7224bb86f7ae97d0273901aeb1282
Edward Hibbert <edward@ehibbert.org.uk>
2025-01-02 17:49:03 +0000

    Experimenting with Perspective API.

--- scripts/fix/fix_toxicity.php
+++ scripts/fix/fix_toxicity.php
@@ -11,7 +11,9 @@ global $dbhr, $dbhm;
 $results = [];
 
 #$newsfeeds = $dbhr->preQuery("SELECT * FROM newsfeed WHERE message IS NOT NULL ORDER BY id DESC LIMIT 100;");
-$newsfeeds = $dbhr->preQuery("SELECT * FROM chat_messages WHERE message IS NOT NULL ORDER BY id DESC LIMIT 100;");
+$newsfeeds = $dbhr->preQuery("SELECT * FROM chat_messages WHERE message IS NOT NULL AND message != '' ORDER BY id DESC LIMIT 1000;");
+
+$count = 0;
 
 foreach ($newsfeeds as $newsfeed) {
     do {
@@ -22,14 +24,29 @@ foreach ($newsfeeds as $newsfeed) {
             $commentsClient->comment(['text' => $newsfeed['message']]);
             $commentsClient->languages(['en']);
             $commentsClient->context(['entries' => ['text' => 'off-topic', 'type' => 'PLAIN_TEXT']]);
-            $commentsClient->requestedAttributes(['TOXICITY' => ['scoreType' => 'PROBABILITY', 'scoreThreshold' => 0]]);
+            $commentsClient->requestedAttributes([
+                'TOXICITY' => ['scoreType' => 'PROBABILITY', 'scoreThreshold' => 0],
+                'SEVERE_TOXICITY' => ['scoreType' => 'PROBABILITY', 'scoreThreshold' => 0],
+                'IDENTITY_ATTACK' => ['scoreType' => 'PROBABILITY', 'scoreThreshold' => 0],
+                'INSULT' => ['scoreType' => 'PROBABILITY', 'scoreThreshold' => 0],
+                'PROFANITY' => ['scoreType' => 'PROBABILITY', 'scoreThreshold' => 0],
+                'THREAT' => ['scoreType' => 'PROBABILITY', 'scoreThreshold' => 0],
+            ]);
             $response = $commentsClient->analyze();
-            $tox = $response->attributeScores()['TOXICITY']['summaryScore']['value'];
+            $tox = 0;
+
+            foreach ($response->attributeScores() as $attribute => $attributeScore) {
+                $val = $attributeScore['summaryScore']['value'];
+                if ($val > $tox) {
+                    $tox = $val;
+                }
+            }
         } catch (\Exception $e) {
             if (strpos($e->getMessage(), 'Quota exceeded')  !== FALSE) {
                 #error_log("...quota exceeded");
                 $sleep = TRUE;
             } else {
+                error_log("Failed on " . $newsfeed['id'] . " " . $newsfeed['message'] . ": " . $e->getMessage());
                 throw $e;
             }
         }
@@ -37,10 +54,16 @@ foreach ($newsfeeds as $newsfeed) {
         if ($sleep) {
             sleep(1);
         }
+
+        $count++;
+
+        if ($count % 100 == 0) {
+            error_log("...$count");
+        }
     } while ($sleep);
 
     if ($tox > 0.3) {
-        #error_log("$tox for " . $newsfeed['id'] . " " . $newsfeed['message']);
+        error_log("$tox for " . $newsfeed['id'] . " " . $newsfeed['message']);
         $results[] = [
             'id' => $newsfeed['id'],
             'message' => $newsfeed['message'],
