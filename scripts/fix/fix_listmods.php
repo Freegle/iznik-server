1720f77fcd1939644764d0491dce68618c9548f0
Edward Hibbert <edward@ehibbert.org.uk>
2025-05-19 18:19:19 +0100

    Script to list mods.

---
+++ scripts/fix/fix_listmods.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Freegle\Iznik;
+
+define('BASE_DIR', dirname(__FILE__) . '/../..');
+require_once(BASE_DIR . '/include/config.php');
+
+require_once(IZNIK_BASE . '/include/db.php');
+global $dbhr, $dbhm;
+
+$mods = $dbhr->preQuery("SELECT DISTINCT(userid) FROM memberships WHERE role IN (?, ?);", [
+    User::ROLE_MODERATOR,
+    User::ROLE_OWNER
+]);
+
+$csv = [
+    [
+        'userid',
+        'email',
+        'name',
+        'lastaccess',
+        'activemod',
+        'backupmod'
+    ]
+];
+
+foreach ($mods as $mod) {
+    $u = new User($dbhr, $dbhm, $mod['userid']);
+    $email = $u->getEmailPreferred();
+    $lastaccess = $u->getPrivate('lastaccess');
+    $lastaccess = $lastaccess ? date('Y-m-d', strtotime($lastaccess)) : null;
+
+    $modships = $u->getMemberships(true);
+    $active = [];
+    $backup = [];
+
+    foreach ($modships as $modship) {
+        #error_log("check {$mod['userid']} on modship " . $modship['id']);
+        $g = Group::get($dbhr, $dbhm, $modship['id']);
+        if ($u->activeModForGroup($modship['id'])) {
+            $active[] = $g->getPrivate('nameshort');
+        } else {
+            $backup[] = $g->getPrivate('nameshort');
+        }
+    }
+
+    $csv[] = [
+        $mod['userid'],
+        $email,
+        $u->getName(),
+        $lastaccess,
+        implode(',', $active),
+        implode(',', $backup)
+    ];
+}
+
+foreach ($csv as $row) {
+    fputcsv(STDOUT, $row);
+}
