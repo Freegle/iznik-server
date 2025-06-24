e4825fc9a4a35efbcb10f316dffb782daa997eef
Edward Hibbert <edward@ehibbert.org.uk>
2025-06-24 16:36:40 +0100

    Prepare for change in Reach Volunteering feed format.

--- scripts/cron/reachvolunteering.php
+++ scripts/cron/reachvolunteering.php
@@ -8,9 +8,21 @@ require_once(BASE_DIR . '/include/config.php');
 require_once(IZNIK_BASE . '/include/db.php');
 global $dbhr, $dbhm;
 
+// Parse command line arguments
+$feedUrl = REACH_FEED;
+$useNewFieldNames = false;
+
+if ($argc > 1) {
+    $feedUrl = $argv[1];
+}
+
+if ($argc > 2) {
+    $useNewFieldNames = ($argv[2] === 'new');
+}
+
 $lockh = Utils::lockScript(basename(__FILE__));
 
-$r = new ReachVolunteering($dbhr, $dbhm);
-$r->processFeed(REACH_FEED);
+$r = new ReachVolunteering($dbhr, $dbhm, $useNewFieldNames);
+$r->processFeed($feedUrl);
 
 Utils::unlockScript($lockh);
