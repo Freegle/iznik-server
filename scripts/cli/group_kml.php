8c9fe65a5b742ff2e836e6234583b6014ea820f7
Edward Hibbert <edward@ehibbert.org.uk>
2025-06-25 09:14:42 +0100

    Add groupid to group kml dump

--- scripts/cli/group_kml.php
+++ scripts/cli/group_kml.php
@@ -24,7 +24,7 @@ $kml = "<?xml version='1.0' encoding='UTF-8'?>
 foreach ($groups as $group) {
     error_log("Group {$group['nameshort']}");
     $geom = \geoPHP::load($group['poly'], 'wkt');
-    $kml .= "<Placemark><name>{$group['nameshort']}</name>" . $geom->out('kml') . "</Placemark>\r\n";
+    $kml .= "<Placemark><name>{$group['nameshort']} #{$group['id']}</name>" . $geom->out('kml') . "</Placemark>\r\n";
 }
 
 $kml .= "		</Folder>
