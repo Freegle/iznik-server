c35f70b858dcf38e2297c1e900bed9766a7ba268
Edward Hibbert <edward@ehibbert.org.uk>
2025-08-14 10:45:51 +0100

    WIP: refactor phpunit tests to have less horrific amounts of duplicate code.

---
+++ scripts/migrate/migrate_central.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../include/config.php';
+require_once(IZNIK_BASE . '/include/db.php');
+
+require_once(IZNIK_BASE . '/include/group/Group.php');
+
+$dsn = "mysql:host={$dbconfig['host']};dbname=republisher;charset=utf8";
+
+$dbhd = new LoggedPDO($dsn, $dbconfig['user'], $dbconfig['pass']);
+
+$g = Group::get($dbhr, $dbhm);
+
+$sql = "SELECT * FROM `groups` WHERE grouppublish = 1;";
+$fgroups = $dbhd->query($sql);
+
+foreach ($fgroups as $fgroup) {
+    error_log("FD group {$fgroup['groupname']}");
+    $id = $g->findByShortName($fgroup['groupname']);
+
+    if ($id) {
+        $g = Group::get($dbhr, $dbhm, $id);
+
+        $settings = $g->getPublic()['settings'];
+        $settings['centralmailsdisabled'] = intval($fgroup['centralmailsdisabled']);
+        $g->setPrivate('settings', json_encode($settings));
+    }
+}
+
