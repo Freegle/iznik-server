2cbcac778c085ca97faeba38c060da85a9512fc1
edwh <edward@ehibbert.org.uk>
2025-10-13 13:31:09 +0100

    Migrate logo API call from v1 to v2

--- http/api/logo.php
+++ http/api/logo.php
@@ -1,6 +1,12 @@
 <?php
 namespace Freegle\Iznik;
 
+// TODO: DEPRECATED - This endpoint has been migrated to v2 Go API
+// Can be retired once all FD/MT clients are using v2
+// Migrated: 2025-10-13
+// V2 endpoints: GET /logo
+// Used by: Both FD and MT for fetching special date-specific logos
+
 function logo() {
     global $dbhr, $dbhm;
 
